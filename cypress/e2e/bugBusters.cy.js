ee9bfdabe3e8788bc2f8aea9547dd17a334bbc18
MaryMoroz <morozova.mary@gmail.com>
2022-11-06 17:57:27 -0500

    add defaultCommandTimeout and new test

--- cypress/e2e/bugBusters.cy.js
+++ cypress/e2e/bugBusters.cy.js
@@ -1,5 +1,14 @@
 describe('groupBugBusters', () => {
+
+    beforeEach(() => {
+        cy.viewport(1280, 720)
+    })
+
     it('AT_003.001 | Search City', function () {
-        cy.visit('https://home.openweathermap.org/')
+        cy.visit('https://openweathermap.org')
+        cy.get('div#desktop-menu a[href="/guide"]').click()
+        cy.get('main.wrapper a[href]:not(.btn-orange)').each(($el) => {
+            cy.wrap($el).should('have.css', 'color').and('eq', 'rgb(233, 110, 80)');
+        })
     })
 })     
