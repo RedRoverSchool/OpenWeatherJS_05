5f21b919d238c034289cb5e668f7c9b7e2465436
MaryMoroz <morozova.mary@gmail.com>
2022-11-07 13:18:16 -0500

    refactor

--- cypress/e2e/bugBusters.cy.js
+++ cypress/e2e/bugBusters.cy.js
@@ -6,9 +6,9 @@ describe('groupBugBusters', () => {
 
     it('AT_016.001 | Verify all links has the same orange color on the page', function () {
         cy.visit('https://openweathermap.org')
-        cy.get('div#desktop-menu a[href*="marketplace"]').invoke('removeAttr', 'target').click()
+        cy.get('div#desktop-menu a[href*="marketplace"]').click()
         cy.get('div.market-place a[href]:not(.button-round)').each(($el) => {
-            cy.wrap($el).should('have.css', 'color').and('eq', 'rgb(235, 110, 75)');
+            cy.wrap($el).should('have.css', 'color', 'rgb(235, 110, 75)');
         })
     })
 })     
