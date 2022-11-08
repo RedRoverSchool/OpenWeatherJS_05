eaff9e17c0b67542c42c03e254ede3d94ce2bb29
MaryMoroz <morozova.mary@gmail.com>
2022-11-08 13:52:18 -0500

    add viewport

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     chromeWebSecurity: false,
     defaultCommandTimeout: 7000,
     e2e: {
--- cypress/e2e/bugBusters.cy.js
+++ cypress/e2e/bugBusters.cy.js
@@ -1,14 +1,22 @@
 describe('groupBugBusters', () => {
 
-    beforeEach(() => {
-        cy.viewport(1280, 720)
-    })
-
     it('AT_016.001 | Verify all links has the same orange color on the page', function () {
         cy.visit('https://openweathermap.org')
         cy.get('div#desktop-menu a[href*="marketplace"]').invoke('removeAttr', 'target').click()
+        //doesn't work
+        // cy.get('div.market-place a[href]:not(.button-round)').eq(1).as('secondLink')
+        // cy.log(cy.get('@secondLink').text())
+
+        // Находим текст одного из элементов массива
+        cy.get('div.market-place a[href]:not(.button-round)').eq(0).then(($el) => {
+            let link1Text = $el.text()
+            cy.log(link1Text)
+        })
+
+        //Пробегаем по всему массиву веб элементов
         cy.get('div.market-place a[href]:not(.button-round)').each(($el) => {
-            cy.wrap($el).should('have.css', 'color', 'rgb(230, 110, 75)');
+            // cy.log($el.text())
+            cy.wrap($el).should('have.css', 'color', 'rgb(235, 110, 75)');
         })
     })
 })     
