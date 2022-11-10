084dc337cb57dd65e5bf39f29598ee4f3ff99a91
MaryMoroz <morozova.mary@gmail.com>
2022-11-09 21:15:38 -0500

    refresh from main

--- cypress/e2e/groupTeachers.cy.js
+++ cypress/e2e/groupTeachers.cy.js
@@ -4,6 +4,7 @@ describe('group jsTeachers', () => {
 
   it('AT_010.001 | Marketplace > Verify all links have the same color on the page', () => {
     cy.visit('https://openweathermap.org')
+    
     cy.get('div#desktop-menu a[href*="marketplace"]').invoke('removeAttr', 'target').click()
 
     cy.get('div.market-place a[href]:not(.button-round)').each(($el, index) => {
