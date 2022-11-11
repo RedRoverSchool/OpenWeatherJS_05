5b66f550c621a75f53e3fdbfe0316c9249c998fa
EllieShalum <ellieshalum@gmail.com>
2022-11-11 10:15:36 -0600

    AT_008005

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -1,17 +1,10 @@
 /// <reference types="cypress"/> 
 
-describe('arkGroup', () => {
-    it(`AT_002.002 | Header > Verifying the website's logo is clickable and it redirects a User to the Main page`, () => {
-        cy.visit('https://openweathermap.org/guide');
-        cy.get('li[class="logo"]').click();
-        cy.url().should('eq', 'https://openweathermap.org/');
-
-    })
-
-    it(`AT_008005 | Main menu > Verify the user be redirected to new URL by clicking "Guide"`, () => {
-
-        cy.visit('https://openweathermap.org/');
-        cy.get('a[href="/guide"]').contains('Guide').click();
-        cy.url().should('eq', 'https://openweathermap.org/guide');
-    })
-})
+describe("arkGroup", () => {
+  it(`AT_008005 | Main menu > Verify the user be redirected to new URL by clicking "Guide"`, () => {
+    cy.visit("https://openweathermap.org/");
+    cy.get('a[href="/guide"]').contains("Guide").click();
+    cy.url().should("eq", "https://openweathermap.org/guide");
+  });
+  
+});
