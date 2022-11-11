bc85e9e52a1fc74ff294b7cb4a545baed50abfe9
Klavdia <kgold94@yahoo.com>
2022-11-11 09:35:35 -0800

    made file with one it

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -5,11 +5,10 @@ describe('all ARK team tests', () => {
       
     it('AT_030.001|Footer>Verify redirection to terms and conditions', function() {
         cy.visit('https://openweathermap.org/')
-    })
-    it("clicked on the link in the footer - the expected page was opened", () => {
+      
         cy.get('div.footer-section a[href*="Openweather_website_terms_and_conditions"]')
         .invoke("removeAttr", "target")
         .click()                  
-        .url().should('include', 'website_terms_and_conditions_of_use.pdf')
+         cy.url().should('include', 'website_terms_and_conditions_of_use.pdf')
     })
 })
