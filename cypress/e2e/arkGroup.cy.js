a150eb6ae669527df6d8c42e62d31b0cd9a17da3
EllieShalum <ellieshalum@gmail.com>
2022-11-11 10:02:33 -0600

    AT_008005

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -7,4 +7,11 @@ describe('arkGroup', () => {
         cy.url().should('eq', 'https://openweathermap.org/');
 
     })
+
+    it(`AT_008005 | Main menu > Verify the user be redirected to new URL by clicking "Guide"`, () => {
+
+        cy.visit('https://openweathermap.org/');
+        cy.get('a[href="/guide"]').contains('Guide').click();
+        cy.url().should('eq', 'https://openweathermap.org/guide');
+    })
 })
