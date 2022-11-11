ea21553701425217e52a0b4626fd67d446d3f238
EllieShalum <ellieshalum@gmail.com>
2022-11-11 10:37:43 -0600

    fix

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -6,21 +6,20 @@ describe("arkGroup", () => {
     cy.visit("https://openweathermap.org/");
     cy.get('a[href="/guide"]').contains("Guide").click();
     cy.url().should("eq", "https://openweathermap.org/guide");
-
-/// <reference types="cypress" />
-
-describe('group Ark', () => {
-
-it('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
-    cy.visit('https://openweathermap.org/')
-    cy.get('#desktop-menu [href*=market]').invoke('removeAttr', 'target').click()
-    cy.get('.market-place .product h5 a').each(el => {
-      cy.wrap(el).should('have.css', 'color','rgb(235, 110, 75)')
-      cy.request(el.prop('href')).should(resp => {
-        expect(resp.status).to.eq(200)
-      })
-    })
-
   });
+});
 
-})
+describe("group Ark", () => {
+  it("AT_010.004 | Marketplace > Verify all orange links on the page", () => {
+    cy.visit("https://openweathermap.org/");
+    cy.get("#desktop-menu [href*=market]")
+      .invoke("removeAttr", "target")
+      .click();
+    cy.get(".market-place .product h5 a").each((el) => {
+      cy.wrap(el).should("have.css", "color", "rgb(235, 110, 75)");
+      cy.request(el.prop("href")).should((resp) => {
+        expect(resp.status).to.eq(200);
+      });
+    });
+  });
+});
