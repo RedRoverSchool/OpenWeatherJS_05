9af6e599ecd079be1f1b567688b73f8bb618d83c
irynaVaku <vakulovskaya@gmail.com>
2022-11-11 14:02:59 -0800

    Add first test

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -1,41 +1,59 @@
 /// <reference types="cypress"/>
 
 describe('Group jScript_group', () => {
+  it('AT_013.001 | NavBar > After clicking the Blog menu User is redirected to the Blog page', () => {
+    cy.visit('https://openweathermap.org');
+    cy.get('#desktop-menu [href*="blog"]')
+      .invoke('removeAttr', 'target')
+      .click();
+    cy.url().should(
+      'be.equal',
+      'https://openweather.co.uk/blog/category/weather'
+    );
+  });
 
-    it('AT_013.001 | NavBar > After clicking the Blog menu User is redirected to the Blog page', () => {
-        cy.visit('https://openweathermap.org');
-        cy.get('#desktop-menu [href*="blog"]').invoke('removeAttr', 'target').click();
-        cy.url().should('be.equal', 'https://openweather.co.uk/blog/category/weather');
-    });
+  it('AT_002.001 | Header > After clicking the logo user is redirected to the home page', () => {
+    cy.visit('https://openweathermap.org/examples');
+    cy.get('.logo').click();
+    cy.url().should('eq', 'https://openweathermap.org/');
+  });
 
-    it('AT_002.001 | Header > After clicking the logo user is redirected to the home page', () => {
-        cy.visit('https://openweathermap.org/examples');
-        cy.get('.logo').click();
-        cy.url().should('eq', 'https://openweathermap.org/');
-     });
+  it('AT_013.002 | NavBar > After redirecting to the Blog page 10 posts are displayed on the first page', () => {
+    cy.visit('https://openweathermap.org');
+    cy.get('#desktop-menu [href*="blog"]')
+      .invoke('removeAttr', 'target')
+      .click();
+    cy.get('.post-list .post').should('have.length', 10);
+  });
 
-    it('AT_013.002 | NavBar > After redirecting to the Blog page 10 posts are displayed on the first page', () => {
-        cy.visit('https://openweathermap.org');
-        cy.get('#desktop-menu [href*="blog"]').invoke('removeAttr', 'target').click();
-        cy.get('.post-list .post').should('have.length', 10);
-    });
+  it('AT_030.001 | Footer > After clicking on the "Website terms and conditions" in the footer the expected page is opened', () => {
+    cy.visit('https://openweathermap.org/');
+    cy.get('[href*="use.pdf"]').invoke('removeAttr', 'target').click();
+    cy.url().should('include', 'terms_and_conditions_of_use.pdf');
+  });
 
-    it('AT_030.001 | Footer > After clicking on the "Website terms and conditions" in the footer the expected page is opened', () => {
-        cy.visit('https://openweathermap.org/');
-        cy.get('[href*="use.pdf"]').invoke('removeAttr', 'target').click();
-        cy.url().should('include','terms_and_conditions_of_use.pdf')
-    });
+  it('AT_017.002 | Support > "How to start" > Verify "How to start" link redirection', () => {
+    cy.visit('https://openweathermap.org/');
+    cy.get('#support-dropdown').click({ force: true });
+    cy.get('.dropdown-menu [href*="/appid"]').click({ force: true });
+    cy.url().should('eq', 'https://openweathermap.org/appid');
+  });
 
-    it('AT_017.002 | Support > "How to start" > Verify "How to start" link redirection', () => {
-        cy.visit('https://openweathermap.org/')
-        cy.get('#support-dropdown').click({force: true})
-        cy.get('.dropdown-menu [href*="/appid"]').click({force: true})
-        cy.url().should('eq', 'https://openweathermap.org/appid') 
-     })
+  it('AT_050.001 | Footer >Terms and conditions of sale', () => {
+    cy.visit('https://openweathermap.org/');
+    cy.get('#footer-website [href*="sale"]')
+      .invoke('removeAttr', 'target')
+      .click();
+    cy.url().should(
+      'eq',
+      'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf'
+    );
+  });
 
-     it('AT_050.001 | Footer >Terms and conditions of sale', () => {
-        cy.visit('https://openweathermap.org/');
-        cy.get('#footer-website [href*="sale"]').invoke('removeAttr', 'target').click();
-        cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf');
-     })
+  it("AT_002.003 | Header > Verifying the website's logo is clickable and redirects User to the Main page", () => {
+    cy.visit('https://openweathermap.org/');
+    cy.get('#desktop-menu a[href="/weathermap"]').click();
+    cy.get('.logo').click();
+    cy.url().should('include', 'https://openweathermap.org/');
+  });
 });
