a72842a47cf1b90e27d3f0f1cca5dd1efec65e01
MaryMoroz <morozova.mary@gmail.com>
2022-11-12 11:22:08 -0500

    ref

--- cypress/e2e/asiaJS.cy.js
+++ cypress/e2e/asiaJS.cy.js
@@ -7,7 +7,7 @@ describe('asiaJS', () => {
   });
 
   it('AT_010.002 | Marketplace > Verify link “History Bulk” are clickable', () => {
-    cy.get('#desktop-menu [href$="/guide"]').click()
+    cy.get('#desktop-menu [href$="marketplace"]').invoke('removeAttr', 'target').click()
     cy.url().should('include', '/marketplace')
     cy.get('.product-container a[href="/history_bulks/new"]:not(.button-round)').click()
     cy.url().should('include', '/history_bulks/new')
