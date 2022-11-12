2f8923cb1be50bd4a6d446fd7ab6cdff808cbdcb
Sana291 <oksy2000@gmail.com>
2022-11-12 11:53:59 -0800

    add AT_004.001

--- cypress/e2e/group_javascript_for_qas.cy.js
+++ cypress/e2e/group_javascript_for_qas.cy.js
@@ -4,4 +4,9 @@ describe('group_javascript_for_qas', () => {
     it('AT_028.001 | Footer > About us > Verify "About us" link redirects to the corresponding page', function () {
         cy.visit('https://openweathermap.org/');
     });
+    it.only('AT_004.001 | Main page > Verify the temperature can be switched from Imperial to Metric', function(){
+        cy.visit('https://openweathermap.org');
+        cy.get('.switch-container > div:nth-of-type(3)').click();
+        cy.get('.switch-container > div:nth-of-type(2)').should('contain', 'Metric: °C, m/s').click();
+  });
 });
