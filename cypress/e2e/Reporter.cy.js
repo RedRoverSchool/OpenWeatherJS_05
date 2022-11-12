93b560cbb2f90e83f3e7375a7273bdc41ccc49a7
nataliiahaiduk <gaidutsyl@gmail.com>
2022-11-11 21:19:11 -0500

    Verify Name and Descrip

--- cypress/e2e/Reporter.cy.js
+++ cypress/e2e/Reporter.cy.js
@@ -13,4 +13,11 @@ describe('GroupReporters', () => {
             .should('have.text', "Not found. To make search more precise put the city's name, comma, 2-letter country code (ISO3166).")
         cy.get('div.widget-notification').should('have.text', `No results for ${inputCity}`)
     })
+
+    it('AT_005.001 | Verify the website name and description', () => {
+        cy.visit('https://openweathermap.org')
+        cy.get('h1 .orange-text').should('have.text', 'OpenWeather')
+        cy.get('h2 .white-text')
+            .should('have.text', 'Weather forecasts, nowcasts and history in a fast and elegant way')
+    })
 })
