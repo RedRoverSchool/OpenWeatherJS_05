e9a030e11192553ea5f30648fd546de02beacc20
nataliiahaiduk <gaidutsyl@gmail.com>
2022-11-12 13:26:31 -0500

    refactor

--- cypress/e2e/Reporter.cy.js
+++ cypress/e2e/Reporter.cy.js
@@ -31,7 +31,6 @@ describe('GroupReporters', () => {
     })
 
     it('AT_005.001 | Verify the website name and description', () => {
-        cy.visit('https://openweathermap.org')
         cy.get('h1 .orange-text').should('have.text', 'OpenWeather')
         cy.get('h2 .white-text')
             .should('have.text', 'Weather forecasts, nowcasts and history in a fast and elegant way')
