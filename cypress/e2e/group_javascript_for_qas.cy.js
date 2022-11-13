431ee177ca34201ee6cb6074e8b8fc1a536278bd
Sana291 <oksy2000@gmail.com>
2022-11-13 09:02:18 -0800

    add AT_004.001  changes to code based on comments

--- cypress/e2e/group_javascript_for_qas.cy.js
+++ cypress/e2e/group_javascript_for_qas.cy.js
@@ -4,10 +4,10 @@ describe('group_javascript_for_qas', () => {
     it('AT_028.001 | Footer > About us > Verify "About us" link redirects to the corresponding page', function () {
         cy.visit('https://openweathermap.org/');
     });
-    
+
     it('AT_004.001 | Main page > Verify the temperature can be switched from Imperial to Metric', function(){
         cy.visit('https://openweathermap.org');
-        cy.get('.switch-container > div:nth-of-type(3)').click();
+        cy.get('.switch-container > div:nth-of-type(3)').should('contain', 'Imperial: °F, mph');
         cy.get('.switch-container > div:nth-of-type(2)').should('contain', 'Metric: °C, m/s').click();
   });
 });
