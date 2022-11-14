2e289e1b792d0ff0e7e6e12575e7b200da4bce81
marinamonroe141 <marinamonroe2021@gmail.com>
2022-11-14 13:37:32 -0500

    refactor

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -1,8 +1,23 @@
-/// <reference types="cypress" />
+/// <reference types = "cypress" />
+
+
+describe('all ARK team tests', () => {
+
+
+  it ('AT_030.001|Footer>Verify redirection to terms and conditions', function() {
+      cy.visit('https://openweathermap.org/')
+  })
+  it ("clicked on the link in the footer - the expected page was opened", () => {
+  cy.get('div.footer-section a[href*="Openweather_website_terms_and_conditions"]')
+  .invoke("removeAttr", "target")
+  .click()                  
+  .url().should('include', 'website_terms_and_conditions_of_use.pdf')
+})
+})
 
 describe('group Ark', () => {
 
-it('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
+    it ('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
     cy.visit('https://openweathermap.org/')
     cy.get('#desktop-menu [href*=market]').invoke('removeAttr', 'target').click()
     cy.get('.market-place .product h5 a').each(el => {
@@ -12,5 +27,33 @@ it('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
       })
     })
   });
-  
+
 });
+
+
+describe('ark group', () => {
+
+  it('AT_033.005 | Header > Navigation > Guide',() => {
+  cy.visit('https://openweathermap.org/')
+  cy.get('div#desktop-menu a[href*="/guide"]').invoke('removeAttr', 'target').click()
+  cy.url().should('include','/guide')
+ 
+  } ) 
+   })
+
+   
+   it('AT_008.006.02 | Main menu > Guide > Verify The text "OpenWeather products" is displayed.', () => {
+    cy.visit('https://openweathermap.org/');
+    cy.get('a[href="/guide"]').contains("Guide").click();
+    cy.get('.wrapper').should('be.visible')
+    cy.get('div[class="col-sm-12"]').contains('OpenWeather products').should('be.visible')
+  })
+
+
+
+
+
+
+
+
+
