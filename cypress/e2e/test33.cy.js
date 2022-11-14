d009e8ae303e4955f3d3ce96045fab698e3beb37
marinamonroe141 <marinamonroe2021@gmail.com>
2022-11-14 11:23:32 -0500

    add test33

---
+++ cypress/e2e/test33.cy.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+
+///cy.visit('https://openweathermap.org/')
+
+describe('ark group', () => {
+
+    it('AT_033.005 | Header > Navigation > Guide',() => {
+    cy.visit('https://openweathermap.org/')
+    cy.get('div#desktop-menu a[href*="/guide"]').invoke('removeAttr', 'target').click()
+    cy.url().should('include','/guide')
+   
+    } ) 
+     })
+
+
+    
+
+
+     
