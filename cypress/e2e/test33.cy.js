878bd69cdbb42e174513673b95877435b2b82a25
MARINA MONROE <mkarimullina@gmail.com>
2022-11-14 16:12:28 -0500

    add test 33

--- cypress/e2e/test33.cy.js
+++ cypress/e2e/test33.cy.js
@@ -8,7 +8,7 @@ describe('ark group', () => {
     it('AT_033.005 | Header > Navigation > Guide',() => {
     cy.visit('https://openweathermap.org/')
     cy.get('div#desktop-menu a[href*="/guide"]').invoke('removeAttr', 'target').click()
-    cy.url().should('include','/guide')
+    cy.url().should('include','/guide');
    
     } ) 
      })
