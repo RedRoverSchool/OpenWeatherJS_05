3740216284e1497a7ea6d3d32f75e44829656621
ralex83 <alexey.rokhmanov@gmail.com>
2022-11-14 14:30:58 -0600

    AR_AT_002.012

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -172,7 +172,7 @@ describe('Group jScript_group', () => {
         cy.url().should('eq', 'https://openweathermap.org/our-initiatives');
     });
 
-    it("AT_002.003 | Header > Checking the website's logo is clickable and redirects User to the Main page", () => {
+    it("AT_002.012 | Header > Checking the website's logo is clickable and redirects User to the Main page", () => {
         cy.visit('https://openweathermap.org/');
         cy.get('#desktop-menu a[href="/weathermap"]').click();
         cy.get('.logo').click();
