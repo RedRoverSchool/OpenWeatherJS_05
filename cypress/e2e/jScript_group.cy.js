10741c4a234f153d04631fe23dbb7b77b58bc860
TimothyHuxx <dreamtim0101@gmail.com>
2022-11-14 22:04:07 -0800

    add semicolon

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -4,8 +4,8 @@ describe('Group jScript_group', () => {
     beforeEach(function() {
         cy.fixture('jScript_group').then(data => {
             this.data = data;
-        })
-        cy.visit('https://openweathermap.org')
+        });
+        cy.visit('https://openweathermap.org');
     })
 
     it('AT_013.001 | NavBar > After clicking the Blog menu User is redirected to the Blog page', function () {
@@ -25,13 +25,13 @@ describe('Group jScript_group', () => {
 
     it('AT_030.001 | Footer > After clicking on the "Website terms and conditions" in the footer the expected page is opened', function () {
         cy.get('[href*="use.pdf"]').invoke('removeAttr', 'target').click();
-        cy.url().should('include','terms_and_conditions_of_use.pdf')
+        cy.url().should('include','terms_and_conditions_of_use.pdf');
     });
 
     it('AT_017.002 | Support > "How to start" > Verify "How to start" link redirection', function () {
-        cy.get('#support-dropdown').click({force: true})
-        cy.get('.dropdown-menu [href*="/appid"]').click({force: true})
-        cy.url().should('eq', 'https://openweathermap.org/appid') 
+        cy.get('#support-dropdown').click({force: true});
+        cy.get('.dropdown-menu [href*="/appid"]').click({force: true});
+        cy.url().should('eq', 'https://openweathermap.org/appid');
     });
 
     it('AT_050.001 | Footer >Terms and conditions of sale', function () {
@@ -52,17 +52,17 @@ describe('Group jScript_group', () => {
     });
 
     it('AT_031.001 | Sign in > Account Dropdown Menu > After cliking the "logout" button the message appears', function () {
-        cy.get('li[class="user-li"] a[href$="sign_in"]').click()
-        cy.get('#user_email').type('3065606@gmail.com')
-        cy.get('#user_password.form-control').type('Qwerty1234')
-        cy.get('[value="Submit"]').click({force: true})
-        cy.get('#desktop-menu #user-dropdown .inner-user-container').click({force: true})
-        cy.get('.dropdown-menu [href*="/sign_out"]').click({force: true})
+        cy.get('li[class="user-li"] a[href$="sign_in"]').click();
+        cy.get('#user_email').type('3065606@gmail.com');
+        cy.get('#user_password.form-control').type('Qwerty1234');
+        cy.get('[value="Submit"]').click({force: true});
+        cy.get('#desktop-menu #user-dropdown .inner-user-container').click({force: true});
+        cy.get('.dropdown-menu [href*="/sign_out"]').click({force: true});
         cy.get('.panel-body').should('have.text', 'You need to sign in or sign up before continuing.');
     });
 
     it('AT_022.001 | Footer > Verification of displayed six Social Media icons', function () {
-        cy.get('.social a').should('have.length', 6).and('be.visible')
+        cy.get('.social a').should('have.length', 6).and('be.visible');
     });
     
     it('AT_033.001 | Header > Navigation > Verify "Dashboard" menu link', function () {
@@ -71,15 +71,15 @@ describe('Group jScript_group', () => {
     });
     
     it('AT_008.002 | Main menu > Guide | Verify the first button "Learn more" is clickable and user will be redirected new url', function () {
-        cy.get("#desktop-menu ul li a[href='/guide']").click()
-        cy.get("ol [href='/api#current']").click()
-        cy.url().should('include', '/api#current')
+        cy.get("#desktop-menu ul li a[href='/guide']").click();
+        cy.get("ol [href='/api#current']").click();
+        cy.url().should('include', '/api#current');
     });
 
     it('AT_025.004 | Header > Verify user will be redirected to new url "/weather-dashboard"', function () {
         let dashboard_button = '#desktop-menu > :nth-child(2) > :nth-child(3) > a'
-        cy.get(dashboard_button).click()
-        cy.url().should('include','weather-dashboard')
+        cy.get(dashboard_button).click();
+        cy.url().should('include','weather-dashboard');
     });
 
     it('AT_022.002 | Footer > Ensure Facebook icon redirection', function () {
@@ -89,7 +89,7 @@ describe('Group jScript_group', () => {
     });
 
     it ('AT_012.001 | Partners > CMS > Verifying 4 buttons exist in the section', function () {
-        cy.visit('https://openweathermap.org/examples')
+        cy.visit('https://openweathermap.org/examples');
         cy.get('#cms a').should(($a) => {
             expect($a).to.have.length(4);
             expect($a.eq(0)).to.contain('See on the website');
@@ -102,7 +102,7 @@ describe('Group jScript_group', () => {
     it('AT_024.001 | Main page > "Different weather?" option > Verify email enter', function () {
         cy.get('#weather-widget span.owm-switch').click();
         cy.get('#dialogDesc div.more-options').click();
-        cy.get('#weather-widget  input[type="email"]').clear().type('test@gmail.com')
+        cy.get('#weather-widget  input[type="email"]').clear().type('test@gmail.com');
     });
     
     it('AT_012.004 | Partners > CMS > Verify "View widget" button', function () {
@@ -112,8 +112,8 @@ describe('Group jScript_group', () => {
     });
 
     it('AT_050.002 | Footer > Verify that user can be redirected to the "Terms and conditions of sale" page', function () {
-        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click()
-        cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf')
+        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click();
+        cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf');
     });
 
     it('AT_012.005 | Partners > CMS > Verify “View plugin” button for WordPress HD Weather Widget by The Waypoint', function () {
@@ -136,12 +136,12 @@ describe('Group jScript_group', () => {
     });
     
     it('AT_045.001 | Main page > Section with 8-day forecast>See the weather forecast for 8 days', function () {
-        cy.get('ul.day-list li').should('have.length', 8)
+        cy.get('ul.day-list li').should('have.length', 8);
     });
 
     it('AT_033.007 | Header > Navigation >> Verify "Pricing" menu link', function () {
-        cy.get('#desktop-menu a[href="/price"]').should('have.text','Pricing').click()
-        cy.url().should('eq','https://openweathermap.org/price')
+        cy.get('#desktop-menu a[href="/price"]').should('have.text','Pricing').click();
+        cy.url().should('eq','https://openweathermap.org/price');
     });
     
     it('AT_022.003 | Footer > Verify Tweeter icon redirection', function () {
@@ -166,14 +166,9 @@ describe('Group jScript_group', () => {
     });
 
     it('AT_013.005 | Blog > Weather > The Road to a New Thinking in Transport Power', function () {
-        cy.get('div#desktop-menu a[href="https://openweather.co.uk/blog/category/weather"]')
-          .invoke('removeAttr', 'target')
-          .click();
-        cy.get('h2.post__title')
-          .contains('The Road to a New Thinking in Transport Power')
-          .click();
-        cy.get('h1.post-page__title')
-          .should('have.text', 'The Road to a New Thinking in Transport Power')
+        cy.get('div#desktop-menu a[href="https://openweather.co.uk/blog/category/weather"]').invoke('removeAttr', 'target').click();
+        cy.get('h2.post__title').contains('The Road to a New Thinking in Transport Power').click();
+        cy.get('h1.post-page__title').should('have.text', 'The Road to a New Thinking in Transport Power');
     });     
     
     it('AT_013.003 | Verifying the first post\'s link is clickable and redirects User to the post on a new page', function () {
@@ -192,7 +187,7 @@ describe('Group jScript_group', () => {
         cy.get('a[href="/about-us"]').click();
         cy.get('a.round[href*="blog"]').invoke('removeAttr', 'target').click();
         cy.url().should('include', '/blog/category/weather');
-    })
+    });
 
     it('AT_015.001 | Header > Support > Ask a question > Not checking eCAPTCHA checkbox', function () {
         cy.get('#support-dropdown').click();
@@ -200,7 +195,7 @@ describe('Group jScript_group', () => {
         cy.get('.headline').should('have.text', 'Ask a question');
 
         cy.get('#question_form_is_user_false').check();
-        cy.get('#question_form_email').type('testemail@gmail.com');
+        cy.get('#question_form_email').type(this.data.email);
         cy.get('#question_form_subject').select('I want to discuss a purchase of OpenWeather products/subscriptions');
         cy.get('#question_form_message').type('My message to the world!');
         cy.get('.btn').click();
