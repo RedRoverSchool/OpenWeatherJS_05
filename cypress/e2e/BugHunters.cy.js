7e5d476039ce05287b292ad91eb9bf40cb92708c
SvetlanaMag <svetlanammmsv@gmail.com>
2022-11-17 19:25:58 +0300

    add first test AT_041.001

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -145,4 +145,18 @@ describe('groupBugHunters', () => {
     cy.get(marketplace).invoke('removeAttr', 'target').click({force: true})
     cy.get(documentationButton).should('be.visible').invoke('removeAttr', 'target').click() 
   })
+
+  it('AT_041.001 | Verify the registered user is redirected to the My API keys page', () => {
+    
+    cy.get('li.user-li a[href*="sign_in"]').click()
+    cy.get('.new_user .email').type('redroverschool@yahoo.com')
+    cy.get('#user_password').type('123456789')
+    cy.get('input[value="Submit"]').click()
+
+    cy.get('#user-dropdown').click()
+    cy.get('#user-dropdown-menu a[href="/api_keys"]').click()
+    cy.url().should('include', '/api_keys')
+    cy.get('div.alert-info').should('contain','You can generate as many API keys as needed for your subscription. We accumulate the total load from all of them.')
+      .and('be.visible')
+  })
 })
