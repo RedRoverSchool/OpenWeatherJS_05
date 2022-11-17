e1dea4efcff4f5350c3e51374dda47e13f925df7
MARINA MONROE <mkarimullina@gmail.com>
2022-11-17 12:48:02 -0500

    add 'AT_033.017

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -176,4 +176,9 @@ it('AT_002.013 | Header > Verifying the Main page is open after clicking the log
 });
 })
 
-
+it('AT_033.017 | Header>Navigation>API>Verify "sign up" link',() => {
+  cy.visit("https://openweathermap.org/")
+  cy.get('#desktop-menu > :nth-child(2) > :nth-child(2)').click()
+  cy.contains("sign up").click()
+  cy.url().should('include','https://home.openweathermap.org/users/sign_up')
+})
