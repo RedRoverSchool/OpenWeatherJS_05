c375d3bacfa200484ae43cad944d1168a4f3df78
shabaiev <ilyashabaev2209@gmail.com>
2022-11-18 19:49:25 -0600

    fixed spaces

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -419,6 +419,10 @@ describe('Group jScript_group', () => {
         cy.get('.day-list li:first-child > span').should('have.text', todaysDate);
     });
 
+    it('AT_045.006 | Main page > Section with 8-day forecast > Verifying the weather forecast for 8 days is displayed in the section', function () {
+        cy.get('.day-list li').should('have.length', this.data.dayListLength);
+    });
+
     it('AT_041.002 | Header > User > My API keys > Verify that user can navigate to api keys page and see alert info message', function () {
         cy.get('.user-li a').click();
         cy.get('[class*="string email optional "]').type(this.data.loginUserEmail);
@@ -429,8 +433,5 @@ describe('Group jScript_group', () => {
 
         cy.url().should('eq', 'https://home.openweathermap.org/api_keys');
         cy.get('.alert-info').should('have.text', '\nYou can generate as many API keys as needed for your subscription. We accumulate the total load from all of them.\n');
-    });
-    it('AT_045.006 | Main page > Section with 8-day forecast > Verifying the weather forecast for 8 days is displayed in the section', function () {
-        cy.get('.day-list li').should('have.length', this.data.dayListLength);
     });
 });
