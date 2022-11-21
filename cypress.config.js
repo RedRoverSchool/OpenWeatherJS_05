16b6f608d695b50846eef662297da96d12d567ef
Mikhail Subbotin <msubbotin78@gmail.com>
2022-11-21 11:55:00 -0700

    adding AT_048.003

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     chromeWebSecurity: false,
-    defaultCommandTimeout: 9000,
+    defaultCommandTimeout: 7000,
     e2e: {
         setupNodeEvents(on, config) {
           // implement node event listeners here
