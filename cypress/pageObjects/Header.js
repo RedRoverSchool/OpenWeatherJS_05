f847174c4333d1410b140a81bbbe93cd5578015e
Rom-T <roman1985r@gmail.com>
2022-11-22 23:14:35 -0500

    transfer test AT_016.001 to POM

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,10 +1,20 @@
 class Header {
     elements = {
-        getBlogMenuLink: () => cy.get('#desktop-menu [href*="blog"]')
+        getBlogMenuLink: () => cy.get('#desktop-menu [href*="blog"]'),
+        getSupportDropDownMenu: () => cy.get('#support-dropdown'),
+        getFAQMenuLink: () => cy.get('ul#support-dropdown-menu a[href="/faq"]')
     }
      
     clickBlogMenuLink() {
         this.elements.getBlogMenuLink().invoke('removeAttr', 'target').click({force: true})
+    }
+
+    clickSupportDropDownMenu() {
+        this.elements.getSupportDropDownMenu().click({force: true})
+    }
+
+    clickFAQMenuLink() {
+        this.elements.getFAQMenuLink().click({force: true})
     }
 }
 export default Header;
