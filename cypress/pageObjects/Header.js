d9941aa47eac1dbbf581a733c502f09f88afc673
Artem Diuzhikov <a.diuzhikov@gmail.com>
2022-11-22 23:47:00 -0500

    refactor a test

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,11 +1,16 @@
 class Header {
     elements = {
         getBlogMenuLink: () => cy.get('#desktop-menu [href*="blog"]'),
-        getPartnersMenuLink: () => cy.get('#desktop-menu a[href="/examples"]')
+        getPartnersMenuLink: () => cy.get('#desktop-menu a[href="/examples"]'),
+        getMapsMenuLink: () => cy.get('#desktop-menu [href="/weathermap"]')
     }
      
     clickBlogMenuLink() {
-        this.elements.getBlogMenuLink().invoke('removeAttr', 'target').click({force: true})
+        this.elements.getBlogMenuLink().invoke('removeAttr', 'target').click({force: true});
+    }
+
+    clickMapsMenuLink() {
+        this.elements.getMapsMenuLink().click({force: true});
     }
 }
 export default Header;
