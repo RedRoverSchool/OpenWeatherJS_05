7d9071f9077bc90869fc5f3ab7a0c46e4d74ef1b
Liliia149 <f.lilly149@gmail.com>
2022-11-23 21:36:18 -0500

    Update footerSpec.cy.js

--- cypress/e2e/footerSpec.cy.js
+++ cypress/e2e/footerSpec.cy.js
@@ -14,7 +14,7 @@ describe('Footer test suite', () => {
     });
 
     it('AT_030.001 | Footer > After clicking on the "Website terms and conditions" in the footer the expected page is opened', function () {
-        footer.clickWebsiteTermsAndConditions();
+        footer.clickWebsiteTermsAndConditionsLink();
         
         cy.url().should('be.equal',this.data.websiteTermsUrl);
     });
