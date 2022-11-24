21919dc905f4634d299dba36cda036c80414ac06
Liliia149 <f.lilly149@gmail.com>
2022-11-23 21:35:26 -0500

    Update Footer.js
    add link in the name of the method

--- cypress/pageObjects/Footer.js
+++ cypress/pageObjects/Footer.js
@@ -1,12 +1,12 @@
 class Footer {
     elements = {
-        getWebsiteTermsAndConditions: () => cy.get('[href$="use.pdf"]'),
+        getWebsiteTermsAndConditionsLink: () => cy.get('[href$="use.pdf"]'),
         getAppStoreLink: () => cy.get('.my-5 a[href*=apple]'),
         getGooglePlayLink: () => cy.get('.my-5 a[href*=google]'),
     };
 
-    clickWebsiteTermsAndConditions() {
-        this.elements.getWebsiteTermsAndConditions().invoke('removeAttr', 'target').click()
+    clickWebsiteTermsAndConditionsLink() {
+        this.elements.getWebsiteTermsAndConditionsLink().invoke('removeAttr', 'target').click()
     };
 }
 export default Footer;
