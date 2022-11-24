5754193cf5d1f4f9bf2583e00844091f326b6875
LIliia149 <f.lilly149@gmail.com>
2022-11-23 21:18:16 -0500

    AT_030.001 Add new method

--- cypress/pageObjects/Footer.js
+++ cypress/pageObjects/Footer.js
@@ -1,8 +1,12 @@
 class Footer {
     elements = {
-        getWebsiteTermsAndConditions: () => cy.get('[href*="use.pdf"]'),
+        getWebsiteTermsAndConditions: () => cy.get('[href$="use.pdf"]'),
         getAppStoreLink: () => cy.get('.my-5 a[href*=apple]'),
         getGooglePlayLink: () => cy.get('.my-5 a[href*=google]'),
-    }
+    };
+
+    clickWebsiteTermsAndConditions() {
+        this.elements.getWebsiteTermsAndConditions().invoke('removeAttr', 'target').click()
+    };
 }
 export default Footer;
