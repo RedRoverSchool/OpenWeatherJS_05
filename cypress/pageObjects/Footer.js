908d3be1490c7cc0dbed439bd8398bbb9aa5311e
Alzhan-B <alzhan.braliyev@gmail.com>
2022-11-24 00:59:28 -0500

    add RF

--- cypress/pageObjects/Footer.js
+++ cypress/pageObjects/Footer.js
@@ -3,6 +3,11 @@ class Footer {
         getWebsiteTermsAndConditions: () => cy.get('[href*="use.pdf"]'),
         getAppStoreLink: () => cy.get('.my-5 a[href*=apple]'),
         getGooglePlayLink: () => cy.get('.my-5 a[href*=google]'),
+        getAboutUsLink: () => cy.get('div#footer-website a[href="/about-us"]')
+    }
+
+    clickAboutUsLink() {
+        this.elements.getAboutUsLink().click({force: true});
     }
 }
 export default Footer;
