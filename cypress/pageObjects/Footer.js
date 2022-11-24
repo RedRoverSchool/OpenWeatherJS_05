17a20b797db99c51dbc1730211876c6fc4dca0ce
Liliia149 <f.lilly149@gmail.com>
2022-11-23 22:35:18 -0500

    Update Footer.js

--- cypress/pageObjects/Footer.js
+++ cypress/pageObjects/Footer.js
@@ -3,6 +3,7 @@ class Footer {
         getWebsiteTermsAndConditionsLink: () => cy.get('[href$="use.pdf"]'),
         getAppStoreLink: () => cy.get('.my-5 a[href*=apple]'),
         getGooglePlayLink: () => cy.get('.my-5 a[href*=google]'),
+        getTermsAndConditionsOfSaleLink: () => cy.get('[href*="conditions_of_sale"]')
     };
 
     clickOnTermsAndConditionsOfSaleLink() {
