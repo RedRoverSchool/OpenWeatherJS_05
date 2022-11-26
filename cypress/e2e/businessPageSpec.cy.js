561e390fe11939585fdc68acce8ca6b8cf2e6c12
Yulia Lopatko <engineeryulia@gmail.com>
2022-11-25 22:09:04 -0700

    first commit

--- cypress/e2e/businessPageSpec.cy.js
+++ cypress/e2e/businessPageSpec.cy.js
@@ -2,6 +2,7 @@
 
 import Header from "../pageObjects/Header.js";
 import BusinessPage from "../pageObjects/BusinessPage.js";
+import AboutUs from "../pageObjects/AboutUsPage.js";
 
 const header = new Header();
 const businessPage = new BusinessPage();
@@ -11,6 +12,9 @@ describe('businessPageSpec', () => {
     beforeEach(function () {
         cy.fixture('businessPage').then(data => {
             this.data = data;
+        });
+        cy.fixture('url').then(url => {
+            this.url = url;
         });
         cy.visit('/');
     })
@@ -26,15 +30,6 @@ describe('businessPageSpec', () => {
 
         cy.url().should('eq', this.data.url)
         businessPage.elements.getH1Title().should('have.text', this.data.h1Title)
-    });
-
-    it('AT_038.002 | For business page > About us', function () {
-        header.clickBusinessMenuLink()
-    
-        cy.get('a.btn_block[href="#main_about"]').click({force: true});
-    
-        cy.url().should('eq', 'https://openweather.co.uk/#main_about');
-        cy.get('h2[style="margin-top: 0;"]').should('include.text', 'OpenWeather products are all');
     });
 });
 
