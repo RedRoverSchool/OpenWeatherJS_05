93e72747bba69b5f8b33efe644d60b723ce8ec2d
StanislavBliss <blissstanislav@gmail.com>
2022-11-27 21:46:55 +0200

    del .only

--- cypress/e2e/footerSpec.cy.js
+++ cypress/e2e/footerSpec.cy.js
@@ -65,7 +65,7 @@ describe('Footer test suite', () => {
             
     });
 
-    it.only("AT_044.004 | Footer > PopUps > Manage cookies > Verify the background color of a button and link when the element is in mouse focus", function () {
+    it("AT_044.004 | Footer > PopUps > Manage cookies > Verify the background color of a button and link when the element is in mouse focus", function () {
         header.clickGuideMenuLink();
 
         cookiesPanel.elements.getCookiesControlElements().each(el => {
