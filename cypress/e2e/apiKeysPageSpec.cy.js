b9eed31fd77f436e7a180970b0c00f75f93ba4ec
yuro4ka250686 <selivanovyurik@gmail.com>
2022-11-27 13:55:39 +0300

    edit

--- cypress/e2e/apiKeysPageSpec.cy.js
+++ cypress/e2e/apiKeysPageSpec.cy.js
@@ -38,9 +38,10 @@ describe('API keys', () => {
 
         //delete created API key       
         apiKeys.elements.getAPIkyes().each(($el) => {
-            if ($el.find(this.data.locators.nameKeys).text() == this.data.keyNames.newNameKey) {
-                cy.wrap($el).find(this.data.locators.deleteKeysButton).click()
+            if ($el.find(apiKeys.elements.locatorNameKeys).text() == this.data.keyNames.newNameKey) {
+                cy.wrap($el).find(apiKeys.elements.locatorDeleteKeysButton).click()
               }
+
             })
     })
 
--- cypress/pageObjects/ApiKeysPage.js
+++ cypress/pageObjects/ApiKeysPage.js
@@ -2,11 +2,15 @@ class ApiKeys {
 
     elements = {
 
+        locatorNameKeys: "td:nth-child(2)",
+        locatorDeleteKeysButton: "td a i.fa-remove",
+        
         getAPIkyes: () => cy.get('.api-keys tbody tr'),
         getNamesAPIkeys: () => cy.get('td:nth-child(2)'),    
         getCreateKeyField: () => cy.get('#api_key_form_name'),
         getGenerateButton: () => cy.get('.button-round[value="Generate"]'),
-        getNotification: () => cy.get('.col-md-6'),        
+        getNotification: () => cy.get('.col-md-6'),  
+              
     }
 
     clickGenerateButton() {
