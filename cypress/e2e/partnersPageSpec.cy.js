e13d96cc3fdc91d4757532d5bed717d7fe974d45
SvetlanaKanel <svetlana.kanel@gmail.come>
2022-11-28 22:28:15 -0500

    Add RF | AT_012.004 (POM) and put header.clickPartnersMenuLink(); to beforeEach

--- cypress/e2e/partnersPageSpec.cy.js
+++ cypress/e2e/partnersPageSpec.cy.js
@@ -3,10 +3,12 @@
 import Header from "../pageObjects/Header.js";
 import PartnersPage from "../pageObjects/PartnersPage.js";
 import DrupalPage from "../pageObjects/DrupalPage.js";
+import WordpressPage from "../pageObjects/WordpressPage.js"
  
 const header = new Header();
 const partnersPage = new PartnersPage();
 const drupalPage = new DrupalPage();
+const wordpressPage = new WordpressPage();
  
 describe('Partners page test suite', () => {
 
@@ -17,29 +19,38 @@ describe('Partners page test suite', () => {
 
         cy.fixture('url').then(url => {
             this.url = url;
-        })
+        });
 
         cy.fixture('drupalPage').then(valueTitle => {
             this.drupalPageTitle = valueTitle;
-        })
+        });
+
+        cy.fixture('wordpressPage').then(textLink => {
+            this.textLink = textLink;
+        }) 
 
         cy.visit('/');
+
+        header.clickPartnersMenuLink();
     });
 
-    it('AT_012.001 | Partners > CMS > Verifying 4 buttons exist in the section', function () {
-        header.clickPartnersMenuLink();
-
+    it('AT_012.001 | Partners > CMS > Verifying 4 buttons exist in the section', function () {       
         partnersPage.elements.getCMSNameButtons().each(($el, i) => {
             expect($el.text()).to.equal(this.data.CMSNameButtons[i]);
         });
     });
 
     it('AT_012.002 | Partners > CMS > Verify "See on the website" button', function() {
-        header.clickPartnersMenuLink();
-
         partnersPage.clickCmsSeeOnTheWebsiteButton();
 
         cy.url().should('eq', this.url.drupalWebsite);
         drupalPage.elements.getHeaderLocator().should('have.attr', 'title', this.drupalPageTitle.headerTitle);        
+    })
+
+    it('AT_012.004 | Partners > CMS > Verify "View widget" button', function () {
+        partnersPage.clickCmsViewWidgetButton();
+
+        cy.url().should('eq', this.url.wordpressWebsite);
+        wordpressPage.elements.getHeaderGetWordPressBtn().should('contains.text', this.textLink.headerGetWordPressLink);
     })
 });
--- cypress/pageObjects/PartnersPage.js
+++ cypress/pageObjects/PartnersPage.js
@@ -1,11 +1,16 @@
 class PartnersPage {
     elements = {
         getCMSNameButtons: () => cy.get('#cms a'),
-        getCmsSeeOnTheWebsiteButton: () => cy.get('#cms a[href="http://drupal.org/project/olowm"]')
+        getCmsSeeOnTheWebsiteButton: () => cy.get('#cms a[href="http://drupal.org/project/olowm"]'),
+        getCmsViewWidgetButton: () => cy.get('#cms a[href="http://wordpress.org/extend/plugins/awesome-weather/"]')
     }
 
     clickCmsSeeOnTheWebsiteButton() {
         this.elements.getCmsSeeOnTheWebsiteButton().invoke('removeAttr', 'target').click();
+    }
+
+    clickCmsViewWidgetButton() {
+        this.elements.getCmsViewWidgetButton().invoke('removeAttr', 'target').click();
     }
 }
 export default PartnersPage;
