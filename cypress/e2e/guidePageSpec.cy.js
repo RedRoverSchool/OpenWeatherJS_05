a702ca065d7bf1516409ae7577f7c55fa403216e
EllieShalum <ellieshalum@gmail.com>
2022-11-28 21:42:00 -0600

    created header and moved AT_008.006 to the header

--- cypress/e2e/guidePageSpec.cy.js
+++ cypress/e2e/guidePageSpec.cy.js
@@ -50,10 +50,4 @@ describe('Guide page test suite', () => {
         cy.url().should('be.equal', this.url.mainPageLink);
     });
 
-    it('AT_008.006 | Main menu > Guide > Verify The text "Weather data in a fast and easy-to-use way" is displayed.', function () {
-        header.clickGuideMenuLink();
-        cy.url().should('be.equal', this.url.guidePage);
-    
-        guidePage.elements.getPageDescription().contains(this.text.pageDescriptionText).should('be.visible')
-      })
 });
--- cypress/e2e/headerSpec.cy.js
+++ cypress/e2e/headerSpec.cy.js
@@ -1,3 +1,31 @@
 /// <reference types="cypress"/>
 
 import Header from "../pageObjects/Header.js";
+import GuidePage from "../pageObjects/GuidePage.js";
+
+const header = new Header();
+const guidePage = new GuidePage();
+
+describe('Header test suit', () => {
+
+    beforeEach(function () {
+        cy.fixture('asiaJS').then(data => {
+            this.data = data
+        });
+        cy.fixture('url').then(url => {
+            this.url = url
+        });
+        cy.fixture('guidePage').then(text => {
+            this.text = text
+        })
+        cy.visit('/');
+    });
+
+    it('AT_008.006 | Main menu > Guide > Verify The text "Weather data in a fast and easy-to-use way" is displayed.', function () {
+        header.clickGuideMenuLink();
+        cy.url().should('be.equal', this.url.guidePage);
+    
+        guidePage.elements.getPageDescription().should('have.text', this.text.pageDescriptionText).and('be.visible')
+      })
+
+})
