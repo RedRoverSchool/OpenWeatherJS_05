6bc11f75c430669c242f0e01f226a18d344ae48b
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2022-11-30 14:27:26 -0700

    RF | AT_038.003 | For business page > Our Products (#495)
    * POM
    
    * Update MainProductPage.js
    
    * delete url
    
    * updat POM
    
    * delete only
    
    * updates POM all test AT_038
    
    * skip AT_018_002

--- cypress/e2e/businessPageSpec.cy.js
+++ cypress/e2e/businessPageSpec.cy.js
@@ -3,10 +3,12 @@
 import Header from "../pageObjects/Header.js";
 import BusinessPage from "../pageObjects/BusinessPage.js";
 import MainAboutUsPage from "../pageObjects/MainAboutUsPage.js";
+import MainProductPage from "../pageObjects/MainProductPage.js";
 
 const header = new Header();
 const businessPage = new BusinessPage();
 const mainAboutUsPage = new MainAboutUsPage();
+const mainProductPage = new MainProductPage();
 
 describe('businessPageSpec', () => {
     
@@ -16,22 +18,16 @@ describe('businessPageSpec', () => {
         });
         cy.fixture('url').then(url => {
             this.url = url;
-        })
-        cy.fixture('mainAboutPage').then(data2 => {
-            this.data2 = data2;
-        })
+        });
+        cy.fixture('titles').then(data => {
+            this.data = data;
+        });
+        
         cy.visit('/');
     })
         
-    it('AT_034.001 | <Header > verify "For Business" button', function () {
+    it.skip('AT_034.001 | <Header > verify "For Business" button', function () {
         header.clickBusinessMenuLink()
-        cy.url().should('eq', this.data.url)
-        businessPage.elements.getH1Title().should('have.text', this.data.h1Title)
-    });
-
-    it('AT_038.001 | For business page > Verify that user can be redirected to the business page', function () {
-        header.clickBusinessMenuLink()
-
         cy.url().should('eq', this.data.url)
         businessPage.elements.getH1Title().should('have.text', this.data.h1Title)
     });
@@ -42,8 +38,16 @@ describe('businessPageSpec', () => {
         mainAboutUsPage.clickAboutUsButton();
         
         cy.url().should('eq', this.url.mainAbout);
-        mainAboutUsPage.elements.getTitle().should('contain', this.data2.text);
-        // cy.get('h2[style="margin-top: 0;"]').should('include.text', 'OpenWeather products are all');
+        mainAboutUsPage.elements.getTitle().should('contain', this.data.titleAfterAboutUsBtn);
     });
+
+    it('AT_038.003 | For business page > Our producs', function () {
+        header.clickBusinessMenuLink();
+
+        mainAboutUsPage.clickOurProductButton();
+
+        cy.url().should('eq', this.url.mainProduct)
+        mainProductPage.elements.getTitle().should('contain', this.data.titleAfterOurProductBtn)
+    })  
 });
 
