2e8d50a465767f6fa2d6ffe84351c8afc7f0417e
Anzhela Khakunova <anzhela.khakunova@gmail.com>
2022-11-29 23:52:34 -0800

    RF_AT_018.002_POM

--- cypress/e2e/headerSpec.cy.js
+++ cypress/e2e/headerSpec.cy.js
@@ -4,10 +4,12 @@
 import GuidePage from "../pageObjects/GuidePage.js";
 import Header from "../pageObjects/Header.js";
 import FAQPage from "../pageObjects/FAQPage.js";
+import HowToStartPage from "../pageObjects/HowToStartPage.js";
 
 const guidePage = new GuidePage();
 const header = new Header();
 const faqPage = new FAQPage();
+const howToStart = new HowToStartPage();
 
 describe('Header test suit', () => {
 
@@ -20,6 +22,9 @@ describe('Header test suit', () => {
         });
         cy.fixture('faqPage').then(faqData => {
             this.faqData = faqData;
+        });
+        cy.fixture('howToStartPage').then(pageText => {
+            this.pageText = pageText
         });
         cy.visit('/');
     });
@@ -37,5 +42,13 @@ describe('Header test suit', () => {
 
         cy.url().should('eq', this.url.FAQPage);
         faqPage.elements.getTitle().should('have.text', this.faqData.h1Title);
+    });
+
+    it('AT_018.002 | Support > Dropdown menu > Verify "How to start" menu link', function() {
+        header.clickSupportDropDownMenu();
+        header.clickSupportHowToStartLink();
+
+        cy.url().should('eq', this.url.howToStartPage);
+        howToStart.elements.getTitle().should('have.text', this.pageText.headerTitle);
     });
 })
