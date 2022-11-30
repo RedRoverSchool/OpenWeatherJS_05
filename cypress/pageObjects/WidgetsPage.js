f47d7d0f4e7ddd1ed1fd6b1b611d3b6da8cdb2a4
yuro4ka250686 <selivanovyurik@gmail.com>
2022-11-30 15:48:47 +0300

    AT_021.007

--- cypress/pageObjects/WidgetsPage.js
+++ cypress/pageObjects/WidgetsPage.js
@@ -4,12 +4,20 @@ class WidgetsPage {
         getPageTitle: () => cy.get('.breadcrumb-title'),
         getApiKeyInputField: () => cy.get('#api-key'),
         getCodeWidgetFirstBtn: () => cy.get('#widget-1-left-brown'),
-        getPopupWindowTitle: () => cy.get('#popup-title')
+        getPopupWindowTitle: () => cy.get('#popup-title'),
+        getAllGetAcodeButtons: () => cy.get('button[id*=brown]'),
+        getPopupWindow: () => cy.get('#popup'),
+        getCopyInBufferButton: () => cy.get('#copy-js-code'),
+        getClosePopupWin: () => cy.get('#popup-close')
     }
 
     clickCodeWidgetFirstBtn() {
         this.elements.getCodeWidgetFirstBtn().click();
     };    
+
+    clickClosePopupWin() {
+        this.elements.getClosePopupWin().click()
+    }
 }
 
 export default WidgetsPage;
