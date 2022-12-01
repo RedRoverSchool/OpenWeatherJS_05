704e1a7bdf7275f469d809f51c362c7111583167
AlexMokeev <alexmokeevmd@gmail.com>
2022-12-01 13:12:18 -0500

    rf

--- cypress/e2e/topicPageSpec.cy.js
+++ cypress/e2e/topicPageSpec.cy.js
@@ -2,23 +2,9 @@
 
 import GuidePage from "../pageObjects/GuidePage.js";
 import Header from "../pageObjects/Header.js";
-import PricingPage from "../pageObjects/PricingPage.js";
-import ApiPage from "../pageObjects/ApiPage.js";
-import FAQPage from "../pageObjects/FAQPage.js";
-import HowToStartPage from "../pageObjects/HowToStartPage.js";
-import BusinessPage from "../pageObjects/BusinessPage.js";
-import MainPage from "../pageObjects/MainPage.js";
-
-
 
 const guidePage = new GuidePage();
 const header = new Header();
-const pricingPage = new PricingPage();
-const apiPage = new ApiPage();
-const faqPage = new FAQPage();
-const howToStart = new HowToStartPage();
-const businessPage = new BusinessPage();
-const mainPage = new MainPage();
 
 describe('Topic Page test suite', () => {
     beforeEach(function () {
