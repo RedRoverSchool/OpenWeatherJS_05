699db1f3d208c831404728a800a97ff5fb9c03fd
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2022-11-30 20:51:16 -0700

    unsckip AT_018.002 (#511)

--- cypress/e2e/headerSpec.cy.js
+++ cypress/e2e/headerSpec.cy.js
@@ -72,7 +72,7 @@ describe('Header test suit', () => {
         guidePage.elements.getTitleGuide().should('have.text', this.text.h1Title);
     });
 
-    it.skip('AT_018.002 | Support > Dropdown menu > Verify "How to start" menu link', function() {
+    it('AT_018.002 | Support > Dropdown menu > Verify "How to start" menu link', function() {
         header.clickSupportDropDownMenu();
         header.clickSupportHowToStartLink();
 
