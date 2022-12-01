512fae42c290007684b993e66787b439fbb78849
Liudmila Portnova <103488693+08AlimDuiL@users.noreply.github.com>
2022-12-01 14:52:51 +0300

    Refactored in POM AT_020.004 (#523)

--- cypress/pageObjects/SignInPage.js
+++ cypress/pageObjects/SignInPage.js
@@ -10,7 +10,9 @@ class SignInPage {
   getFieldForEmailPasswordReset: () => cy.get('div.pwd-lost #user_email'),
   getBtnSendEmailResetPassword: () => cy.get('div.pwd-lost [type = "submit"]'),
   getForgotYourPassword: () =>  cy.get('div.panel-body > span'),
-  getAllert : () => cy.get('.panel.panel-red .panel-body')
+  getAllert : () => cy.get('.panel.panel-red .panel-body'),
+  getCheckRememberMe: () => cy.get('[type="checkbox"]#user_remember_me'),
+  getNoticeAfterSigned: () => cy.get('.panel-body')
   };
 
   typeEnterEmail(userEmail) {
@@ -43,5 +45,16 @@ class SignInPage {
     this.elements.getBtnSendEmailResetPassword().click({force: true})
   }
 
+  checkRememberMe() {
+    this.elements.getCheckRememberMe().check();
+  }
+
+  signInWithRememberMe(email, password) {
+    this.typeEnterEmail(email);
+    this.typeEnterPassword(password);
+    this.checkRememberMe();
+    this.clickSubmitButton();
+  };
+  
 };
 export default SignInPage;
