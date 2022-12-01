2e2c21ea17c5d6e9c0929c65694aaa335ce7defd
IrynaRo <rokhmanova@gmail.com>
2022-11-30 21:45:09 -0600

    Refactored AT028.001

--- cypress/e2e/aboutUsPageSpec.cy.js
+++ cypress/e2e/aboutUsPageSpec.cy.js
@@ -55,4 +55,9 @@ describe('About Us', () => {
         marketplacePage.elements.getMarketplacePageTitle().should('be.visible')
     });
 
+    it.only('AT_028.001 | About us > Verify "About us" link redirects to the corresponding page', function () {
+        footer.clickAboutUsLink();
+        cy.url().should('include', '/about-us');
+    });
+
 });
