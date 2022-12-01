944c4b1c42845b92dee16b25ab43e77d4ee7e7c4
EkaterinaRomankova <catia.romankova@yandex.by>
2022-12-01 16:11:18 +0300

    second_commit

--- cypress/e2e/paymentPageSpec.cy.js
+++ cypress/e2e/paymentPageSpec.cy.js
@@ -54,11 +54,12 @@ describe('Payment page test suite', () => {
       });
 
       it('AT_042.002 | User page >My payments>Verify that text displayed on the page', function () {
-        header.clickSignInMenuLink();
+        header.clickSignInMenuLink()
         signInPage.signIn(this.userProfile.userProfileLtByJS.realEmail, this.userProfile.userProfileLtByJS.password)
         signInPage.elements.getSignOutAllert().should('be.visible').and('have.text', this.userProfile.signInSuccessful)
-        header.clickUserDropDownMenu();
-        header.clickPaymentMenuLink();
+        header.clickUserDropDownMenu()
+        header.clickPaymentMenuLink()
+
         paymentPage.elements.getColomnText().should('have.length', 4).each(($el, i) => {
             expect($el.text()).to.include(this.paymentsTable.paymentsTable[i])
         })       
