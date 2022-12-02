77f800f78ab91ddb274137fbcc89a20eba1b5bf9
Ekaterina Kaverina <64539616+KEkaterinaK@users.noreply.github.com>
2022-12-01 23:04:17 -0500

    AT_033.013_Pricing (#543)
    Co-authored-by: KatieKaverina <ekaterinakaverina.qa@gmail.com>
    Co-authored-by: Rom-T <93357374+Rom-T@users.noreply.github.com>

--- cypress/pageObjects/TopicPage.js
+++ cypress/pageObjects/TopicPage.js
@@ -2,6 +2,7 @@ class TopicPage {
 
     elements = {
     getHomePageButton: () => cy.get('.breadcrumb a[href="/"]'),
+    getPageTitle: () => cy.get('.breadcrumb-title'),
     getHeadlineGuide: () => cy.get('.breadcrumb-title')
     }
 
