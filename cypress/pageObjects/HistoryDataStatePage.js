44b387149a3d7e74916bd876b9a9ca39da0d3852
Anakondos <anakondos@gmail.com>
2022-12-02 00:46:53 -0500

    update

--- cypress/pageObjects/HistoryDataStatePage.js
+++ cypress/pageObjects/HistoryDataStatePage.js
@@ -2,11 +2,6 @@ class HistoryDataStatePage {
 	elements = {
 		getHistoryDataStateTitle:() => cy.get('.breadcrumb-title'),
 		getStateNameArray: () => cy.get('.material-table tbody td:first-child')
-
-	}
-
-	checkNumberOfStates() {
-
 	}
 }
 export default HistoryDataStatePage;
