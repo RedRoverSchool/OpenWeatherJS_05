0f203e020106feae0fb050675a6bd5067cdf3991
EllieShalum <ellieshalum@gmail.com>
2022-12-02 15:14:51 -0600

    AT_008.006.02

--- cypress/e2e/headerSpec.cy.js
+++ cypress/e2e/headerSpec.cy.js
@@ -197,4 +197,11 @@ describe('Header test suit', () => {
         cy.url().should('eq', this.url.mainPageLink);
         mainPage.elements.getPageDescriptionWhiteText().should('have.text', this.mainPageData.pageDescriptionWhiteText);
     });
+
+    it('AT_008.006.02 | Main menu > Guide > Verify The text "OpenWeather products" is displayed.', function () {
+        header.clickGuideMenuLink();
+
+        cy.url().should('eq', this.url.guidePage)
+        guidePage.elements.getOpenWeatherText().should('have.text', this.text.openWeatherText)
+      })
 });
