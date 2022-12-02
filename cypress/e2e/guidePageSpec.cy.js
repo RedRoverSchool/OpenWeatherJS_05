e6e9047c1da85b6bf1bb8cac93138b3c9de8955a
Janyl17 <jkypchakova17@gmail.com>
2022-12-02 18:30:47 +0300

    change in fixture file AT_008.003

--- cypress/e2e/guidePageSpec.cy.js
+++ cypress/e2e/guidePageSpec.cy.js
@@ -13,9 +13,6 @@ const apiPage = new ApiPage();
 
 describe('Guide page test suite', () => {
     beforeEach(function () {
-        cy.fixture('asiaJS').then(data => {
-            this.data = data
-        });
         cy.fixture('url').then(url => {
             this.url = url
         });
@@ -32,10 +29,10 @@ describe('Guide page test suite', () => {
     });
 
     it('AT_008.003 | Main menu > Guide | Verifying the link on the page "Guide"', function () {
-        header.elements.getGuideMenuLink().should('contain.text', this.data.menuLink.guide.text);
+        header.elements.getGuideMenuLink().should('contain.text', this.text.h1Title);
         header.clickGuideMenuLink(); 
 
-        cy.url().should('include', this.data.menuLink.guide.endPoint);
+        cy.url().should('include', this.url.guidePage);
         guidePage.elements.getTitleGuide().should('be.visible');
     });
     
