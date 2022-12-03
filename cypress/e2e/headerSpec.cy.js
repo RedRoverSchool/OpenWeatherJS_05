de5a6d4d33e422c6ebd640f4fb58c57974b81060
Janyl17 <101883788+Janyl17@users.noreply.github.com>
2022-12-03 22:11:39 +0300

    add to headerSpec file and delete in guidePageSpec (#584)

--- cypress/e2e/headerSpec.cy.js
+++ cypress/e2e/headerSpec.cy.js
@@ -212,4 +212,12 @@ describe('Header test suit', () => {
         cy.url().should('eq', this.url.FAQPage)
         faqPage.elements.getTitle().should('have.text', this.faqData.h1Title)
       })
+
+    it('AT_008.003 | Main menu > Guide | Verifying the link on the page "Guide"', function () {
+        header.elements.getGuideMenuLink().should('contain.text', this.text.h1Title);
+        header.clickGuideMenuLink(); 
+
+        cy.url().should('include', this.url.guidePage);
+        guidePage.elements.getTitleGuide().should('be.visible');
+    });
 });
