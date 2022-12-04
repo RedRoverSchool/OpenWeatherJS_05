065a3118e77402a44ac24fee1d58a3f42161f761
Rom-T <93357374+Rom-T@users.noreply.github.com>
2022-12-04 01:15:42 -0500

    add AT_016.003 (#588)

--- cypress/pageObjects/FAQPage.js
+++ cypress/pageObjects/FAQPage.js
@@ -4,6 +4,7 @@ class FAQPage {
         getHowToGetAnApiKeyQuestion: () => cy.get('#how-to-get-an-API-key .question-heading'),
         getHowToGetAnApiKeyQuestionAfterClicking: () => cy.get('#how-to-get-an-API-key .question.visible'),
         getHowToGetAnApiKeyQuestionContent: () => cy.get('#how-to-get-an-API-key .question-content'),
+        getQuestionsElement: () => cy.get('div.row .question-heading')
     };
 
     clickHowToGetAnApiKeyQuestion() {
