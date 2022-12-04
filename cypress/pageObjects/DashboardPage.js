7a5e09e51e0523ab5543b1fcc62ca49e695b8d7a
Mila fox D <milafoxwork@gmail.com>
2022-12-04 16:33:54 +0200

    RF_AT_025.009

--- cypress/pageObjects/DashboardPage.js
+++ cypress/pageObjects/DashboardPage.js
@@ -1,6 +1,11 @@
 class DashboardPage {
     elements = {
-        getWeatherDashboardTitle: () => cy.get('h1.breadcrumb-title')
+        getWeatherDashboardTitle: () => cy.get('h1.breadcrumb-title'),
+        getTryTheDashboardFirstButton: () => cy.get('.col-lg-6 .btn_like')
+    }
+
+    clickTryTheDashboardFirstButton () {
+        this.elements.getTryTheDashboardFirstButton().invoke('removeAttr', 'target').click()
     }
 }
 export default DashboardPage;
