b74814d528e110b3a25c4d6749ee3dccc4dd33c4
yuro4ka250686 <selivanovyurik@gmail.com>
2022-12-04 15:31:36 +0300

    AT_061.005

--- cypress/e2e/historicalWeatherDataByStateSpec.cy.js
+++ cypress/e2e/historicalWeatherDataByStateSpec.cy.js
@@ -3,7 +3,6 @@
 import Header from "../pageObjects/Header";
 import MarketplacePage from "../pageObjects/MarketplacePage";
 import HistoricalWeatherDataByStatePage from "../pageObjects/HistoricalWeatherDataByStatePage";
-import { all } from "cypress/types/bluebird";
 
 const heder = new Header();
 const marketplace = new MarketplacePage();
@@ -19,35 +18,27 @@ describe ('historical wether page', function() {
         cy.visit('/');
     })
 
-    it('AT_061.005 |Marketplace > Historical Data Archives > Historical Weather Data by State >Verifying that each state has its own ZIP code', function() {
+    it('AT_061.005 | Historical Weather Data by State > Verifying that each state has its own ZIP code and particular price', function() {
         heder.clickMarketplaceMenuLink()
         marketplace.clickHistoricalDataArchivesDocumentationLink()
         cy.url().should('contain', this.data.urn)
         historicalWeather.elements.getTitlePage().should('have.text', this.data.titlePage)
-               
-        historicalWeather.elements.getFullListOfStates().then(($name) => {
-            let nameState = $name
+            
+        historicalWeather.elements.getDataState().then(($el) => {
+            const dataStates = $el
                 .toArray()
-                .map(el => el.innerText.trim())
-
-            nameState.forEach((el, i) => {
-                expect(el).to.eql(this.data.dataStates[i][0])
-            })       
-        })                  
-                  
-        historicalWeather.elements.getFullListOfZIPcodes().then(($code) => {
-            const zipCode = $code
-                .toArray()
-                .map(el => el.innerText.trim())
-
-            zipCode.forEach((el, i) => {
-                expect(el).to.eql(this.data.dataStates[i][1])
-            })         
+                .map(el => el.innerText.split('\t'))
+           
+            dataStates.forEach((el, i, arr) => {
+                    //check name of State
+                expect(arr[i][0]).to.eq(this.data.dataStates[i][0])
+                    //check ZIP code
+                expect(arr[i][1]).to.eq(this.data.dataStates[i][1])
+                    //check price
+                expect(arr[i][2]).to.eq(this.data.dataStates[i][2])
+            })
         })
-
-
-    })
-              
- })
+    })              
+})
 
 
