36a8277ef3dcc387d0d3d7f88d741f87fbec85eb
Natalia Kazakova <npkazakova@gmail.com>
2022-12-06 00:05:25 +0700

    RF_010.005

--- cypress/e2e/marketplacePageSpec.cy.js
+++ cypress/e2e/marketplacePageSpec.cy.js
@@ -123,9 +123,17 @@ describe('Marketplace page test suite', () => {
 
       it('AT_010.003 | Marketplace > Verify link “History Forecast Bulk” are clickable', function () {
             header.clickMarketplaceMenuLink();
-            cy.url().should('include', this.urls.MarketPage);        
+            cy.url().should('eql', this.urls.MarketPage);        
             
             marketplacePage.clickHistoryForecastBulk();
             cy.url().should('include', this.urls.historyForecastBulk);
           });
+
+      it('AT_010.005 | Marketplace > Verify link “Historical Weather Data by State for all ZIP codes, USA” are clickable', function () {
+            header.clickMarketplaceMenuLink(); 
+            cy.url().should('eql', this.urls.MarketPage);
+      
+            marketplacePage.clickHistoricalWeatherData ()
+            cy.url().should('include', this.urls.historicalWeatherData);
+      });
 });
