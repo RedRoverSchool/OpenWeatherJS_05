7514501523c6de36b3203369e8790525b07b5b4c
ElviraKh1 <vira2015@aol.com>
2022-12-07 10:37:54 -0500

    Test reqv: create booking and resp:  verify it

--- cypress/e2e/api_testing/apiElviraKh1Spec.cy.js
+++ cypress/e2e/api_testing/apiElviraKh1Spec.cy.js
@@ -1,60 +1,57 @@
 /// <reference types="cypress"/>
 
 const API_BASE_URL = Cypress.env('apiBaseUrl')
-let CREATED_ID
+let BOOKING_ID
 
 let firsNname = "Kia"
 let lastName = "Motors"
-let totalPrice =200
+let totalPrice = 200
 let checkIn = "2020-03-15"
 let checkOut = "2021-03-15"
 let additionalNeeds = "Parking"
 
 describe("API with Cypress", () => {
 
-        const getResponse = () => 
-            cy.request({
-                method: "POST",
-                url: `${API_BASE_URL}/booking`,
-                headers: {
-                    "Content-Type": "application/json"
+    const getResponse = () =>
+        cy.request({
+            method: "POST",
+            url: `${API_BASE_URL}/booking`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                "firstname": firsNname,
+                "lastname": lastName,
+                "totalprice": totalPrice,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": checkIn,
+                    "checkout": checkOut
                 },
-                body: {
-                    "firstname" : firsNname,
-                    "lastname" : lastName,
-                    "totalprice" : totalPrice,
-                    "depositpaid" : true,
-                    "bookingdates" : {
-                        "checkin" : checkIn,
-                        "checkout" : checkOut
-                    },
-                    "additionalneeds" : additionalNeeds
-                }
-            })
+                "additionalneeds": additionalNeeds
+            }
+        })
 
-    
-    
-        it('API | Print response: Create booking', () => {
-            getResponse()
+    it('API | Print response status: create booking confirmation', () => {
+        getResponse()
             .then(response => {
                 console.log(response)
                 expect(response.status).to.equal(200)
             })
-        })
+    })
 
-        it('verify response has key bookingid', () => {
-            getResponse()
+    it('API Verify response:  created booking has fileds bookingid and object booking ', () => {
+        getResponse()
             .its('body')
             .then(response => {
                 expect(response).to.have.any.keys('bookingid')
-                CREATED_ID = response.bookingid
-                console.log('CREATED_ID = ', CREATED_ID)
-                expect(response).to.have.any.keys('booking.firstname')
-                console.log('First Name: ', response.booking.firstname)
-                expect(response).to.have.any.keys('booking.bookingdates.checkin')
-                console.log('Date: ', response.booking.bookingdates.checkin)
-                
+                BOOKING_ID = response.bookingid
+                cy.log('BOOKING_ID = ' + BOOKING_ID)
+
+                expect(response).to.have.any.keys('booking')
+                cy.log('Name: ' + response.booking.firstname + " " + response.booking.lastname)
+                cy.log('Dates: ' + response.booking.bookingdates.checkin + "-" + response.booking.bookingdates.checkin)
+                cy.log('Options: ' + response.booking.additionalneeds)
             })
-        })  
     })
 })
