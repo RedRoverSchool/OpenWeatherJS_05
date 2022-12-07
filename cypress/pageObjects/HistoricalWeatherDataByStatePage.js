ec43872e8c40933d0ec335648c576f872d5a1622
SvetlanaMag <svetlanammmsv@gmail.com>
2022-12-07 22:03:30 +0300

    AT_061.007 Historical Weather Data

--- cypress/pageObjects/HistoricalWeatherDataByStatePage.js
+++ cypress/pageObjects/HistoricalWeatherDataByStatePage.js
@@ -5,7 +5,8 @@ class HistoricalWeatherDataByStatePage {
         getFullListOfPrices: () =>cy.get('.material-table td:last-child'),
         getTitlePage: () => cy.get('.breadcrumb-title'),
         getFullListOfZIPcodes: () => cy.get('.material-table td:nth-child(2)'),
-        getDataState: () => cy.get('.material-table tr:not(tr:first-child)')
+        getDataState: () => cy.get('.material-table tr:not(tr:first-child)'),
+        getDataStateTable: () => cy.get('.material-table')
     };
 };
 export default HistoricalWeatherDataByStatePage
