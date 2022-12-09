049254f5e696177aaf06c61901c73d37a07d87a3
yuro4ka250686 <selivanovyurik@gmail.com>
2022-12-09 19:42:13 +0300

    API_GET

---
+++ cypress/e2e/api_testing/selivanovYurySpec.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress"/>
+
+const API_BASE_URL = Cypress.env('apiBaseUrl')
+
+describe ('testing API', function() {
+
+    beforeEach(function () {
+        cy.fixture('apiData').then(data => {
+            this.data = data;            
+        });
+    });
+
+    describe ('Get method testing', function() {
+
+        const getResponse = () =>
+            cy.request({
+                method: "GET",
+                url: `${API_BASE_URL}/booking`
+            })
+        
+
+        it('verify satatus code and status text', function() {
+            getResponse()
+                .then(({status}) => {
+                    expect(status).to.eq(200)
+                })
+        })
+
+        it('verify there are all headers', function() {
+            getResponse()
+                .then(({headers}) => {   
+                    let hedersKeys = []                 
+                    for(let key in headers) {
+                       hedersKeys.push(key)
+                    }  
+
+                hedersKeys.forEach((el, i, arr) => {
+                    expect(arr[i].toLowerCase()).to.eq(this.data.getHeaders[i].toLowerCase())
+                    })  
+                })           
+        })
+        
+        it('verify response has duration', function() {
+            getResponse()
+                .then((response) => {
+                    expect(response).to.have.property('duration')
+                })
+        })
+    })
+})
