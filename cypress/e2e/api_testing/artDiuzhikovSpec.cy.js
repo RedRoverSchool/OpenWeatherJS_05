6efb21c1fe14ccfc037f8be09e80b57a30e20e5f
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2022-12-08 22:00:41 -0500

    refactor tests (#642)

--- cypress/e2e/api_testing/artDiuzhikovSpec.cy.js
+++ cypress/e2e/api_testing/artDiuzhikovSpec.cy.js
@@ -7,72 +7,57 @@ let CREATION_ID;
 
 describe("artDiuzhikovSpec", function () {
 
-    describe("Create a booking test suite", function () {
-
-        const createABooking = () =>
-            cy.request({
-                method: "POST",
-                url: `${API_BASE_URL}/booking`,
-                headers: {
-                    "Content-Type": "application/json"
+    const createABooking = () =>
+        cy.request({
+            method: "POST",
+            url: `${API_BASE_URL}/booking`,
+            body: {
+                "firstname": "Ivanka",
+                "lastname": "Roust",
+                "totalprice": 214,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2022-06-06",
+                    "checkout": "2019-06-12"
                 },
-                body: {
-                    "firstname": "Ivanka",
-                    "lastname": "Roust",
-                    "totalprice": 214,
-                    "depositpaid": true,
-                    "bookingdates": {
-                        "checkin": "2022-06-06",
-                        "checkout": "2019-06-12"
-                    },
-                    "additionalneeds": "Duck feather pillows"
-                }
-            });
+                "additionalneeds": "Duck feather pillows"
+            }
+        });
+
+    const getTheBooking = () =>
+        cy.request(`${API_BASE_URL}/booking/${CREATION_ID}`);
+
+    describe("Create a booking test suite", function () {
 
         it('Verify the status of the booking creation', function () {
             createABooking()
                 .then(createdBooking => {
                     expect(createdBooking).to.have.property('status', API_DATA.statusOk);
                     CREATION_ID = createdBooking.body.bookingid;
-            });
+                });
         });
 
         it('Verify the last name in the latest created booking', function () {
-            cy.get(CREATION_ID)
-                .then(creationID => {
-                    cy.request(`${API_BASE_URL}/booking/${creationID[0]}`)
-                        .then(createdBooking => {
-                            expect(createdBooking.body.lastname).to.be.equal('Roust');
-                        });
+            getTheBooking()
+                .then(createdBooking => {
+                    expect(createdBooking.body.lastname).to.be.equal('Roust');
                 });
         });
 
         it('Verify the price in the latest created booking', function () {
-            cy.get(CREATION_ID)
-                .then(creationID => {
-                    cy.request(`${API_BASE_URL}/booking/${creationID[0]}`)
-                        .then(createdBooking => {
-                            expect(createdBooking.body.totalprice).to.be.equal(214);
-                        });
+            getTheBooking()
+                .then(createdBooking => {
+                    expect(createdBooking.body.totalprice).to.be.equal(214);
                 });
         });
     });
 
     describe("Get the booking test suite", function () {
 
-        const getTheBooking = () =>
-            cy.get(CREATION_ID)
-                .then(creationID => {
-                    cy.request(`${API_BASE_URL}/booking/${creationID[0]}`);
-                });
-
         it('Verify the booking ID', function () {
             getTheBooking()
                 .then(booking => {
-                    cy.get(CREATION_ID)
-                        .then(creationID => {
-                            expect(booking.allRequestResponses['0']['Request URL']).to.include(creationID[0]);
-                        });
+                    expect(booking.allRequestResponses['0']['Request URL']).to.include(CREATION_ID);
                 });
         });
 
