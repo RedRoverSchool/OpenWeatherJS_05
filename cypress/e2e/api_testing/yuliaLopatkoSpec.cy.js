d4e2c2b3044f57f5d988e03807210c47496a52ec
Yulia Lopatko <engineeryulia@gmail.com>
2022-12-09 11:39:25 -0700

    fix code

--- cypress/e2e/api_testing/yuliaLopatkoSpec.cy.js
+++ cypress/e2e/api_testing/yuliaLopatkoSpec.cy.js
@@ -1,30 +1,33 @@
 /// <reference types="cypress"/>
 
-const API_BASE_URL = Cypress.env("apiBaseUrl")
+const API_BASE_URL = Cypress.env('apiBaseUrl')
 const apiData = require('../../fixtures/apiData.json')
+let CREATED_ID
 
 describe("yuliaLopatkoSpec", () => {
 
-  describe("Create booking", () => {
-    const createBookingRequest = () =>
-      cy.request({
-        method: "POST",
-        url: `${API_BASE_URL}/booking`,
-        header: {
-          "Content-Type": "application/json"
-        },
-        body: {
-          "firstname": apiData.firstname,
-          "lastname": apiData.lastname,
-          "totalprice": apiData.totalprice,
-          "depositpaid": apiData.depositpaid,
-          "bookingdates": {
-            "checkin": apiData.checkin,
-            "checkout": apiData.checkout,
-          },
-          "additionalneeds": apiData.additionalneeds,
-        },
-      })
+  describe('Create booking', () => {
+
+		const createBookingRequest = () =>
+			cy.request({
+				method: "POST",
+				url: `${API_BASE_URL}/booking`,
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: {
+					"firstname": apiData.firstname,
+					"lastname": apiData.lastname,
+					"totalprice": apiData.totalprice,
+					"depositpaid": apiData.depositpaid,
+					"bookingdates": {
+						"checkin": apiData.bookingdates.checkin,
+						"checkout": apiData.bookingdates.checkout
+					},
+
+					"additionalneeds": apiData.additionalneeds
+				}
+			})
 
     it("verify that request creates booking", () => {
       createBookingRequest().then((response) => {
@@ -32,14 +35,14 @@ describe("yuliaLopatkoSpec", () => {
       })
     })
 
-    describe("Get booking Ids", () => {
-      const getBookingIds = () => cy.request(`${API_BASE_URL}/booking`)
+  describe("Get booking Ids", () => {
+    const getBookingIds = () => cy.request(`${API_BASE_URL}/booking`)
 
-      it("verify response status", () => {
-        getBookingIds().then((response) => {
-          expect(response.status).to.eq(200)
-          cy.log(response.body)
-        })
+    it("verify response status", () => {
+      getBookingIds().then((response) => {
+        expect(response.status).to.eq(200)
+        cy.log(response.body)
+      })
       })
     })
   })
