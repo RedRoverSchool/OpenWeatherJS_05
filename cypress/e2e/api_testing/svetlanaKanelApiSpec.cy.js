bc78f6b7fcb8cd276c2812a25a7cd0956b8db6ae
SvetlanaKanel <svetlana.kanel@gmail.come>
2022-12-10 18:15:43 -0500

    All methods remove to the first describe and some logs are deleted

--- cypress/e2e/api_testing/svetlanaKanelApiSpec.cy.js
+++ cypress/e2e/api_testing/svetlanaKanelApiSpec.cy.js
@@ -5,38 +5,46 @@ let CREATED_ID;
 
 describe("svetlanaKanelApiSpec", () => {
 
-    describe("Create booking", () => {
-
-        const getResponse = () =>
-            cy.request({
-                method: "POST",
-                url: `${API_Base_URL}/booking`,
-                headers: {
-                    "Content-Type": "application/json"
+    const createBooking = () =>
+        cy.request({
+            method: "POST",
+            url: `${API_Base_URL}/booking`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                "firstname": "Jim",
+                "lastname": "Brown",
+                "totalprice": 111,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2018-01-01",
+                    "checkout": "2019-01-01"
                 },
-                body: {
-                    "firstname": "Jim",
-                    "lastname": "Brown",
-                    "totalprice": 111,
-                    "depositpaid": true,
-                    "bookingdates": {
-                        "checkin": "2018-01-01",
-                        "checkout": "2019-01-01"
-                    },
-                    "additionalneeds": "Breakfast"
-                }
-            })
+                "additionalneeds": "Breakfast"
+            }
+        });
+
+    const getCreatedBooking = () =>
+        cy.request(`${API_Base_URL}/booking/${CREATED_ID}`);
+
+    const getAllBookings = () =>
+        cy.request({
+            method: "GET",
+            url: `${API_Base_URL}/booking`
+        });
+
+    describe("Create a booking test suite", () => {
 
         it('check response', () => {
-            getResponse()
-                .then(response => {
-                    console.log("Sveta =", response.body);
+            createBooking()
+                .then(response => {                    
                     expect(response.status).to.equal(200);
                 })
         })
 
-        it('verify response has key', () => {
-            getResponse()
+        it('verify that response has key "bookingid" and assign the bookingid to the global veraible CREATED_ID', () => {
+            createBooking()
                 .its('body')
                 .then(response => {
                     expect(response).to.have.any.key('bookingid');
@@ -48,11 +56,8 @@ describe("svetlanaKanelApiSpec", () => {
 
     describe("Get created booking ID", () => {
 
-        const getResponse = () =>
-            cy.request(`${API_Base_URL}/booking/${CREATED_ID}`);
-
         it('verify response has headers', () => {
-            getResponse()
+            getCreatedBooking()
                 .then(response => {
                     console.log(response);
                     expect(response).to.have.property('headers');
@@ -60,37 +65,30 @@ describe("svetlanaKanelApiSpec", () => {
         })
 
         it('verify response has status', () => {
-            getResponse()
+            getCreatedBooking()
                 .its('status')
                 .should('be.eq', 200)
         })
 
         it('verify response is an object', () => {
-            getResponse()
+            getCreatedBooking()
                 .its('body')
                 .should('be.an', 'object');
         })
 
         it('verify response contains an object with key additionalneeds', () => {
-            getResponse()
+            getCreatedBooking()
                 .its('body')
-                .then(response => {
-                    console.log('Sveta2', response);
+                .then(response => {                    
                     expect(response).to.include.keys('additionalneeds');
                 })
         })
-    })    
+    })
 
     describe("Get all BookingsIds", () => {
 
-        const getResponse = () =>
-            cy.request({
-                method: "GET",
-                url: `${API_Base_URL}/booking`
-            })
-
         it('verify response has headers', () => {
-            getResponse()
+            getAllBookings()
                 .then(response => {
                     console.log(response)
                     expect(response).to.have.property('headers')
@@ -98,19 +96,19 @@ describe("svetlanaKanelApiSpec", () => {
         })
 
         it('verify response has status', () => {
-            getResponse()
+            getAllBookings()
                 .its('status')
                 .should('be.eq', 200)
         })
 
         it('verify response is array', () => {
-            getResponse()
+            getAllBookings()
                 .its('body')
                 .should('be.an', 'array')
         })
 
         it('verify response contains object with key bookingid', () => {
-            getResponse()
+            getAllBookings()
                 .its('body')
                 .then(response => {
                     expect(response[0]).to.have.keys('bookingid')
