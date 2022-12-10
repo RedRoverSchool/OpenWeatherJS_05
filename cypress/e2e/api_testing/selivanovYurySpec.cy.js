4b2e2f4f7f8e3a31d7ded97e28c48bbede478084
yuro4ka250686 <selivanovyurik@gmail.com>
2022-12-10 17:10:26 +0300

    post

--- cypress/e2e/api_testing/selivanovYurySpec.cy.js
+++ cypress/e2e/api_testing/selivanovYurySpec.cy.js
@@ -1,50 +1,72 @@
 /// <reference types="cypress"/>
 
 const API_BASE_URL = Cypress.env('apiBaseUrl')
+const apiData = require('../../fixtures/apiData.json')
 
-describe ('selivanovYurySpec', function() {
+describe('selivanovYurySpec', function () {
 
-    beforeEach(function () {
-        cy.fixture('apiData').then(data => {
-            this.data = data;            
-        });
-    });
-
-    describe ('Get method testing', function() {
+    describe('Get method testing', function () {
 
         const getResponse = () =>
             cy.request({
                 method: "GET",
                 url: `${API_BASE_URL}/booking`
             })
-        
 
-        it('verify satatus code', function() {
+
+        it('verify satatus code', function () {
             getResponse()
-                .then(({status}) => {
+                .then(({ status }) => {
                     expect(status).to.eq(200)
                 })
         })
 
-        it('verify there are all headers', function() {
+        it('verify there are all headers', function () {
             getResponse()
-                .then(({headers}) => {   
-                    let hedersKeys = []                 
-                    for(let key in headers) {
-                       hedersKeys.push(key)
-                    }  
+                .then(({ headers }) => {
+                    let hedersKeys = []
+                    for (let key in headers) {
+                        hedersKeys.push(key)
+                    }
 
-                hedersKeys.forEach((el, i, arr) => {
-                    expect(arr[i].toLowerCase()).to.eq(this.data.getHeaders[i].toLowerCase())
-                    })  
-                })           
+                    hedersKeys.forEach((el, i, arr) => {
+                        expect(arr[i].toLowerCase()).to.eq(apiData.getHeaders[i].toLowerCase())
+                    })
+                })
         })
-        
-        it('verify response has duration', function() {
+
+        it('verify response has duration', function () {
             getResponse()
                 .then((response) => {
                     expect(response).to.have.property('duration')
                 })
         })
+    })
+
+    describe('Post method testing', function () {
+
+        const getResponse = () =>
+            cy.request({
+                method: "POST",
+                url: `${API_BASE_URL}/booking`,
+                headers: apiData.headersContentType,
+                body: apiData.bodyCreateBooking
+            })
+
+        it('verify response satus', function () {
+            getResponse()
+                .then(({ status }) => {
+                    expect(status).to.eq(200)
+                })
+        })
+
+        it('verify response body data', function () {
+            getResponse()
+                .then((response) => {
+                    expect(response.body.booking.firstname).to.eq(apiData.bodyCreateBooking.firstname)
+                    expect(response.body.booking.lastname).to.eq(apiData.bodyCreateBooking.lastname)
+                })
+        })
+
     })
 })
