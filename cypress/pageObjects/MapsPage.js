962cbe80b4dfdd06898c41e0db1f3c04f455d620
Illia Shabaiev <ilyashabaev2209@gmail.com>
2022-12-11 16:46:44 -0600

    AT_026.002  > maps visualization of data on the map (#662)
    * AT_026.002
    
    * added test for AT_026.002 + some selectors
    
    * fixed braces
    
    * refactoring
    
    * removed duplicated locators

--- cypress/pageObjects/MapsPage.js
+++ cypress/pageObjects/MapsPage.js
@@ -6,9 +6,17 @@ class MapsPage {
         getCityData: () => cy.get('.expanded > :nth-child(1) > .city-data > .city-full-info > table > tbody'),
         getTemperatureLabel: () => cy.get('label[for=Temperature]'),
         getGlobalPrecipitationLabel: () => cy.get('label[for="Global Precipitation"]'),
-        getScale: () => cy.get('.scale-details'),
+        getScale: () => cy.get('.scale-details'), 
+        getSearchIcon: () => cy.get('[class="leaflet-bar leaflet-control"] div'),
+        getNameOfCity: () => cy.get('span.city-name'),
+        getTableCountryValue: () => cy.get('tbody tr:nth-child(1) .city-param:visible'),
+        getTableTempValue: () => cy.get('tbody tr:nth-child(2) .city-param:visible'),
+        getTableCloudsValue: () => cy.get('tbody tr:nth-child(3) .city-param:visible'),
+        getTableHumidityValue: () => cy.get('tbody tr:nth-child(4) .city-param:visible'),
+        getTablePressureValue: () => cy.get('tbody tr:nth-child(5) .city-param:visible'),
+        getTableWindDirectionValue: () => cy.get('tbody tr:nth-child(6) .city-param:visible'),
+        getTableWindeSpeedValue: () => cy.get('tbody tr:nth-child(7) .city-param:visible'),
         getWindSpeedLabel: () => cy.get('label[for="Wind speed"]'),
-        getSearchIcon: () => cy.get('div.leaflet-control-container a[title="Nominatim Search"]'),
         getInputField: () => cy.get('div.leaflet-control-container form input'),
         getTagWithCityName: () => cy.get('div.city-data span.city-name') ,
         getCityNameExpandedInfo:() => cy.get('div.expanded thead th')        
@@ -16,6 +24,26 @@ class MapsPage {
      
     clickPressureLabel() {
         this.elements.getPressureLabel().click({force: true});
+    }
+
+    clickCityName() {
+        this.elements.getCityName().click();
+    }
+
+    clickGlobalPrecipitationLabel() {
+        this.elements.getGlobalPrecipitationLabel().click();
+    }
+
+    clickOnSearchIcon() {
+        this.elements.getSearchIcon().click();
+    }
+
+    typeSearchWord(cityName) {
+        this.elements.getSearchInput().type(cityName)
+    }
+
+    waitForInputDisappear() {
+        this.elements.getInputField().should('not.be.visible')
     }
 
     clickCityName() {
