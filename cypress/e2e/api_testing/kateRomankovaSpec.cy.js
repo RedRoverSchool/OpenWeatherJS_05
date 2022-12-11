234010b0bb6525bc53a85bc43b7b71244285569d
EkaterinaRomankova <catia.romankova@yandex.by>
2022-12-11 16:58:39 +0300

    delete_booking

--- cypress/e2e/api_testing/kateRomankovaSpec.cy.js
+++ cypress/e2e/api_testing/kateRomankovaSpec.cy.js
@@ -3,7 +3,7 @@
 const API_BASE_URL = Cypress.env('apiBaseUrl')
 const apiData = require('../../fixtures/apiData.json')
 let BOOKING_ID
-let TOKEN
+let token1
 
 describe('kateRomankovaSpec', () => {
 
@@ -69,6 +69,82 @@ describe('kateRomankovaSpec', () => {
                 expect(response.body).has.property('firstname', apiData.firstname)
                 expect(response.body).has.property('lastname', apiData.newInformation.lastname)
                 expect(response.body).has.property('totalprice', apiData.newInformation.totalprice)
+            })          
+        })
+    })
+
+    describe('delete booking', () => {
+
+        const getResponseWithToken = () => 
+        cy.request({
+            method: "POST",
+            url: `${API_BASE_URL}/auth`,
+            body :{
+               "username" : "admin",
+               "password" : "password123"
+            }  
+        })
+
+        const createBooking = () =>
+        cy.request({
+            method: "POST",
+            url: `${API_BASE_URL}/booking`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                "firstname" : apiData.firstname,
+                "lastname" : apiData.lastname,
+                "totalprice" : apiData.totalprice,
+                "depositpaid" : apiData.depositpaid,
+                "bookingdates" : {
+                    "checkin" : apiData.bookingdates.checkin,
+                    "checkout" : apiData.bookingdates.checkout
+                },
+                "additionalneeds" : apiData.additionalneeds
+            }
+        })
+        
+        const deleteBooking = () =>
+        cy.request({
+            method: "DELETE",
+            url: `${API_BASE_URL}/booking/${BOOKING_ID}`,
+            headers: {
+                "Content-Type": "application/json", "Cookie": `token = ${token1}`
+            }
+       })
+       
+       it('create bookingID', () => {
+            createBooking() 
+                .then(response => {
+                    expect(response.status).to.equal(200)
+                    BOOKING_ID = response.body.bookingid
+                })           
+        })
+
+        it('verify response body has key token and send it to the global variable TOKEN', () => {
+			getResponseWithToken()
+				.then(response => {
+					expect(response.body).to.have.key('token')
+					token1 = response.body.token
+				})
+		})
+
+        it('delete booking', () => {
+            deleteBooking()
+                .then(response => {
+                    expect(response.status).to.equal(201)
+                    expect(response.body).to.equal(apiData.responsebobyafterdeletebooking)
+                })          
+        })
+
+        it('verify there is not the created ID', function () {
+            cy.request({
+                method: 'GET',
+                url: `${API_BASE_URL}/booking/${BOOKING_ID}`,
+                failOnStatusCode: false
+            }).then(response => {
+                expect(response.status).to.equal(404)
             })          
         })
     })
