972d8d67e0ccc37ae5d9ad04819aa9c6586784d0
IrinaDash <irina.lev@list.ru>
2022-12-11 21:42:47 +0300

    API_IrinaDashk_ AUTH

--- cypress/e2e/api_testing/IrinadashkSpec.cy.js
+++ cypress/e2e/api_testing/IrinadashkSpec.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress"/>
 
-const API_BASE_URL = Cypress.env('apiBaseUrl')
 
+const API_BASE_URL = Cypress.env('apiBaseUrl')
+let CREATED_ID
+let TOKEN
 describe('IrinadashkSpec', () => {
 
-      describe('GET all bookingIds', () => {
+      describe.skip('GET all bookingIds', () => {
             
             const getResponse = () => 
                   cy.request({
@@ -40,4 +42,91 @@ describe('IrinadashkSpec', () => {
                   })
             })
       })
+
+      describe('Greate Booking', () => {
+            
+            const getResponse = () => 
+                  cy.request({
+                        method:"Post",
+                        url: `${API_BASE_URL}/booking`,
+                        headers: {
+                              "Content-Type": "application/json"
+                        },
+                        body: {
+                              "firstname" : "Jim",
+                              "lastname" : "Brown",
+                              "totalprice" : 111,
+                              "depositpaid" : true,
+                              "bookingdates" : {
+                                    "checkin" : "2018-01-01",
+                                    "checkout" : "2019-01-01"
+                              },
+                              "additionalneeds" : "Breakfast"
+                        }
+                  })
+            
+            it('Verify response has status 200', () => {
+                  getResponse()
+                  .its('status')
+                  .should('be.eq', 200)
+            })
+
+            it('Print response', () => {
+                  getResponse()
+                  .then(response => {
+                  console.log(response.body)
+                  expect(response.status).to.equal(200)
+                  })
+            })  
+            
+            it('Print response has key bookingid', () => {
+                  getResponse()
+                  .its('body')
+                  .then(response => {
+                  expect(response).to.have.any.keys('bookingid') 
+                  CREATED_ID = response.bookingid
+                  console.log('CREATED_ID = ', CREATED_ID)
+                  })
+            })  
+      })
+      
+      describe('AUTH', () => {
+            
+            const getResponse = () => 
+                  cy.request({
+                        method:"Post",
+                        url: `${API_BASE_URL}/auth`,
+                        headers: {
+                              "Content-Type": "application/json"
+                        },
+                        body: {
+                              "username" : "admin",
+                              "password" : "password123"
+                        }
+                  })
+            
+            it('Verify response has status 200', () => {
+                  getResponse()
+                  .its('status')
+                  .should('be.eq', 200)
+            })
+
+            it('Print response', () => {
+                  getResponse()
+                  .then(response => {
+                  console.log(response.body)
+                  expect(response.status).to.equal(200)
+                  })
+            })  
+            
+            it('Print response has key token', () => {
+                  getResponse()
+                  .its('body')
+                  .then(response => {
+                  expect(response).to.have.any.keys('token') 
+                  TOKEN = response.token
+                  console.log('TOKEN = ', TOKEN)
+                  })
+            }) 
+      }) 
 })
