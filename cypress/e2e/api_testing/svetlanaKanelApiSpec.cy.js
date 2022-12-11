a5c10d7fc0b5de823b4ab4b2a208577aa1796e51
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2022-12-10 22:42:57 -0500

    svetlanaKanelApiSpec | create token > update booking (#667)
    * All methods remove to the first describe and some logs are deleted
    
    * Creat token, update booking and test them
    
    Co-authored-by: SvetlanaKanel <svetlana.kanel@gmail.come>

--- cypress/e2e/api_testing/svetlanaKanelApiSpec.cy.js
+++ cypress/e2e/api_testing/svetlanaKanelApiSpec.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress"/>
 
-const API_Base_URL = Cypress.env('apiBaseUrl');
+const API_BASE_URL = Cypress.env('apiBaseUrl');
 let CREATED_ID;
+let CREATED_TOKEN;
 
 describe("svetlanaKanelApiSpec", () => {
 
     const createBooking = () =>
         cy.request({
             method: "POST",
-            url: `${API_Base_URL}/booking`,
+            url: `${API_BASE_URL}/booking`,
             headers: {
                 "Content-Type": "application/json"
             },
@@ -26,19 +27,54 @@ describe("svetlanaKanelApiSpec", () => {
         });
 
     const getCreatedBooking = () =>
-        cy.request(`${API_Base_URL}/booking/${CREATED_ID}`);
+        cy.request(`${API_BASE_URL}/booking/${CREATED_ID}`);
 
     const getAllBookings = () =>
         cy.request({
             method: "GET",
-            url: `${API_Base_URL}/booking`
+            url: `${API_BASE_URL}/booking`
         });
+
+    const createToken = () =>
+        cy.request({
+            method: "POST",
+            url: `${API_BASE_URL}/auth`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: {
+                "username": "admin",
+                "password": "password123"
+            }
+        });
+
+    const updateBooking = () => 
+        cy.request({
+            method: "PUT",
+            url: `${API_BASE_URL}/booking/${CREATED_ID}`,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Cookie": `token=${CREATED_TOKEN}`
+            },
+            body: {
+                "firstname": "Jumanji",
+                "lastname": "Brown",
+                "totalprice": 111,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2018-01-01",
+                    "checkout": "2019-01-01"
+                },
+                "additionalneeds": "Breakfast + aperol spritz"
+            }
+        })
 
     describe("Create a booking test suite", () => {
 
         it('check response', () => {
             createBooking()
-                .then(response => {                    
+                .then(response => {
                     expect(response.status).to.equal(200);
                 })
         })
@@ -79,11 +115,41 @@ describe("svetlanaKanelApiSpec", () => {
         it('verify response contains an object with key additionalneeds', () => {
             getCreatedBooking()
                 .its('body')
-                .then(response => {                    
+                .then(response => {
                     expect(response).to.include.keys('additionalneeds');
                 })
         })
     })
+
+    describe('Create a new auth token to use for access to the PUT and DELETE/booking', () => {
+
+        it('verify that token is created and assigh it to the global varible CREATED_TOKEN', () => {
+            createToken()
+                .its('body')
+                .then(response => {
+                    expect(response).to.have.keys('token');
+                    CREATED_TOKEN = response.token;
+                    console.log("CREATED_TOKEN = ", CREATED_TOKEN);
+                });
+        });
+    });
+
+    describe('Booking - UpdateBooking', () => {
+        
+        it('Update booking and verifay status', () => {
+            updateBooking()
+            .its('status')
+            .should('be.equal', 200)
+        });
+
+        it('veryfy new updations', () => {
+            updateBooking()
+            .its('body')
+            .then(response => {
+                expect(response.firstname).to.eq("Jumanji");
+            })            
+        })
+    });
 
     describe("Get all BookingsIds", () => {
 
