32fb5e46cad9ae24000ee99c24b5a630e03a9135
YurySelivanov <105432578+YurySelivanov@users.noreply.github.com>
2022-12-11 16:49:22 +0300

    create (#670)

--- cypress/e2e/api_testing/selivanovYurySpec.cy.js
+++ cypress/e2e/api_testing/selivanovYurySpec.cy.js
@@ -3,6 +3,9 @@
 const API_BASE_URL = Cypress.env('apiBaseUrl')
 const apiData = require('../../fixtures/apiData.json')
 
+let TOKEN
+let ID
+
 describe('selivanovYurySpec', function () {
 
     describe('Get method testing', function () {
@@ -68,5 +71,69 @@ describe('selivanovYurySpec', function () {
                 })
         })
 
+        it('verify response has duration', function () {
+            getResponse()
+                .then(({ body }) => {
+                    expect(body).to.have.any.keys('bookingid')
+                    ID = body.bookingid
+                })
+        })
+
+    })
+
+    describe('create token', function () {
+
+        const getResponse = () =>
+            cy.request({
+                method: "POST",
+                url: `${API_BASE_URL}/auth`,
+                headers: apiData.headersContentType,
+                body: apiData.admin
+            })
+
+        it('verify response satus', function () {
+            getResponse()
+                .then(({ status }) => {
+                    expect(status).to.eql(200)
+                })
+        })
+
+        it('verify response has token', function () {
+            getResponse()
+                .then(({ body }) => {
+                    expect(body).to.have.key('token')
+                    TOKEN = body.token
+                })
+        })
+    })
+
+    describe('delete booking', function () {
+
+        const getResponse = () =>
+            cy.request({
+                method: "DELETE",
+                url: `${API_BASE_URL}/booking/${ID}`,
+                headers: {
+                    "Content-Type": "application/json",
+                    'Cookie': `token=${TOKEN}`
+                }
+            })
+
+        it('verify response satus', function () {
+            getResponse()
+                .then(({ status }) => {
+                    expect(status).to.eql(201)
+                })
+        })
+
+        it('verify there is not the created ID', function () {
+            cy.request({
+                method: 'GET',
+                url: `${API_BASE_URL}/booking/${ID}`,
+                failOnStatusCode: false
+            }).then(({ status }) => {
+                expect(status).to.eql(404)
+            })
+        })
     })
 })
