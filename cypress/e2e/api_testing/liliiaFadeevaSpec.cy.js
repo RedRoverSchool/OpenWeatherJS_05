b44069f76fc257adb41487a3cd659598917fd6fb
LIliia149 <f.lilly149@gmail.com>
2022-12-12 00:28:29 -0500

    add api test

--- cypress/e2e/api_testing/liliiaFadeevaSpec.cy.js
+++ cypress/e2e/api_testing/liliiaFadeevaSpec.cy.js
@@ -5,10 +5,10 @@ const API_DATA = require('../../fixtures/apiData.json');
 let CREATED_ID;
 
 describe('liliiaFadeevaSpec', () => {
-    
+
     describe('Create a booking test suite', () => {
 
-        const createBooking = () => 
+        const createBooking = () =>
             cy.request({
                 method: "POST",
                 url: `${API_BASE_URL}/booking`,
@@ -16,15 +16,15 @@ describe('liliiaFadeevaSpec', () => {
                     "Content-Type": "application/json"
                 },
                 body: {
-                    "firstname" : API_DATA.firstname,
-                    "lastname" : API_DATA.lastname,
-                    "totalprice" : API_DATA.totalprice,
-                    "depositpaid" : API_DATA.depositpaid,
-                    "bookingdates" : {
-                        "checkin" : API_DATA.bookingdates.checkin,
-                        "checkout" : API_DATA.bookingdates.checkout
+                    "firstname": API_DATA.firstname,
+                    "lastname": API_DATA.lastname,
+                    "totalprice": API_DATA.totalprice,
+                    "depositpaid": API_DATA.depositpaid,
+                    "bookingdates": {
+                        "checkin": API_DATA.bookingdates.checkin,
+                        "checkout": API_DATA.bookingdates.checkout
                     },
-                    "additionalneeds" : API_DATA.additionalneeds
+                    "additionalneeds": API_DATA.additionalneeds
                 }
             });
 
@@ -34,7 +34,7 @@ describe('liliiaFadeevaSpec', () => {
                     expect(response.status).to.equal(API_DATA.statusOk)
                 });
         });
-    
+
         it('Verify the response has key bookingid', () => {
             createBooking()
                 .its('body')
@@ -57,5 +57,43 @@ describe('liliiaFadeevaSpec', () => {
                     expect(response.body.booking.firstname).to.be.a('string');
                 });
         });
-    }); 
+    });
+
+    describe('GET booking test suite', () => {
+
+        const getResponse = () =>
+            cy.request({
+                method: "GET",
+                url: `${API_BASE_URL}/booking`
+            });
+
+        it('Verify response status', () => {
+            getResponse()
+                .then(({ status }) => {
+                    expect(status).to.be.eq(API_DATA.statusOk);
+                });
+        });
+
+
+        it('Verify response is array', () => {
+            getResponse()
+                .its('body')
+                .should('be.an', 'array');
+        });
+
+        it('Verify response has headers', () => {
+            getResponse()
+                .then(response => {
+                    console.log(response);
+                    expect(response).to.have.property('headers');
+                });
+        });
+
+        it('Verify response has bookingid key', () => {
+            getResponse()
+                .then(({ body }) => {
+                    expect(body[0]).to.have.keys('bookingid');
+                });
+        });
+    });
 });
