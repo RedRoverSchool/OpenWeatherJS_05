47e63d3ef3607f6143c60db79c43d9e8e3d47f98
LIliia149 <f.lilly149@gmail.com>
2022-12-12 00:33:20 -0500

    Add Get booking test suite

--- cypress/e2e/api_testing/yuliaLopatkoSpec.cy.js
+++ cypress/e2e/api_testing/yuliaLopatkoSpec.cy.js
@@ -1,33 +1,30 @@
 /// <reference types="cypress"/>
 
-const API_BASE_URL = Cypress.env('apiBaseUrl')
-const apiData = require('../../fixtures/apiData.json')
+const API_BASE_URL = Cypress.env("apiBaseUrl")
+const apiData = require("../../fixtures/apiData.json")
 let CREATED_ID
 
 describe("yuliaLopatkoSpec", () => {
-
-  describe('Create booking', () => {
-
-		const createBookingRequest = () =>
-			cy.request({
-				method: "POST",
-				url: `${API_BASE_URL}/booking`,
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: {
-					"firstname": apiData.firstname,
-					"lastname": apiData.lastname,
-					"totalprice": apiData.totalprice,
-					"depositpaid": apiData.depositpaid,
-					"bookingdates": {
-						"checkin": apiData.bookingdates.checkin,
-						"checkout": apiData.bookingdates.checkout
-					},
-
-					"additionalneeds": apiData.additionalneeds
-				}
-			})
+  describe("Create booking", () => {
+    const createBookingRequest = () =>
+      cy.request({
+        method: "POST",
+        url: `${API_BASE_URL}/booking`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          firstname: apiData.firstname,
+          lastname: apiData.lastname,
+          totalprice: apiData.totalprice,
+          depositpaid: apiData.depositpaid,
+          bookingdates: {
+            checkin: apiData.bookingdates.checkin,
+            checkout: apiData.bookingdates.checkout,
+          },
+          additionalneeds: apiData.additionalneeds,
+        },
+      })
 
     it("verify that request creates booking", () => {
       createBookingRequest().then((response) => {
@@ -35,14 +32,26 @@ describe("yuliaLopatkoSpec", () => {
       })
     })
 
-  describe("Get booking Ids", () => {
-    const getBookingIds = () => cy.request(`${API_BASE_URL}/booking`)
+    describe("Get booking Ids", () => {
+      const getBookingIds = () => cy.request(`${API_BASE_URL}/booking`)
 
-    it("verify response status", () => {
-      getBookingIds().then((response) => {
-        expect(response.status).to.eq(200)
-        cy.log(response.body)
+      it("verify response status", () => {
+        getBookingIds().then((response) => {
+          expect(response.status).to.eq(200)
+        })
       })
+    })
+  })
+  describe.only("Get booking by filter lastname", () => {
+    const filterByLastName = () =>
+      cy.request({
+        method: "GET",
+        url: `${API_BASE_URL}/booking?lastname=${apiData.lastname}`,
+      })
+
+    it("verify status", () => {
+      filterByLastName().then((response) => {
+        expect(response.status).to.be.equal(200)
       })
     })
   })
