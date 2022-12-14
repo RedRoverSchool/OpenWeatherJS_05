16c3f2e49777c07ea20a2057fd76c197ea801272
OLgaBuR <olbur12@gmail.com>
2022-12-13 22:30:20 -0600

    first commit

---
+++ cypress/pageObjects/UserMyProfilePage.js
@@ -0,0 +1,26 @@
+class UserMyProfilePage {
+	elements = {
+		getMyProfileTabmenu: () => cy.get('#myTab a[href="/home"]'),
+		getNewPasswordImput: () => cy.get('#password_form_password'),
+		getConfirmNewPassword: () => cy.get('#password_form_password_confirmation'),
+		getChangePasswordBtn: () => cy.get('input[value="Change Password"]'),
+		getAllertMessage: () => cy.get('.panel-body'),
+	};
+
+	clickMyProfileTabmenu() {
+		this.elements.getMyProfileTabmenu().click()
+	};
+
+	clickChangePaswordBtn() {
+		this.elements.getChangePasswordBtn().click()
+	};
+
+	passwordChange(newpassword) {
+		this.clickMyProfileTabmenu()
+		this.elements.getNewPasswordImput().type(newpassword, { log: false })
+		this.elements.getConfirmNewPassword().type(newpassword, { log: false })
+		this.clickChangePaswordBtn()
+	};
+};
+
+export default UserMyProfilePage;
