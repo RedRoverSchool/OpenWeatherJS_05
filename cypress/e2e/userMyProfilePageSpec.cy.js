071d3963a0af47e841ce9b0917aa5f7eaf9beb35
OLgaBuR <olbur12@gmail.com>
2022-12-14 10:41:40 -0600

    move clickMyProfileTabmenu() to UserHomePage

--- cypress/e2e/userMyProfilePageSpec.cy.js
+++ cypress/e2e/userMyProfilePageSpec.cy.js
@@ -3,10 +3,12 @@
 import Header from "../pageObjects/Header";
 import SignInPage from "../pageObjects/SignInPage";
 import UserMyProfilePage from "../pageObjects/UserMyProfilePage";
+import UserHomePage from "../pageObjects/UserHomePage";
 
 const header = new Header();
 const signInPage = new SignInPage();
 const usermyprofile = new UserMyProfilePage();
+const userhomepage = new UserHomePage();
 
 describe('Change password test suit', () => {
 
@@ -21,6 +23,7 @@ describe('Change password test suit', () => {
 		header.clickSignInMenuLink()
 		signInPage.signIn(this.data.userProfileLtByJS.realEmail, this.data.userProfileLtByJS.password)
 
+		userhomepage.clickMyProfileTabmenu()
 		usermyprofile.passwordChange(this.data.userProfileLtByJS.newpassword)
 
 		usermyprofile.elements.getAllertMessage().should('have.text', this.data.messageSuccessPasswordChange)
--- cypress/pageObjects/UserMyProfilePage.js
+++ cypress/pageObjects/UserMyProfilePage.js
@@ -1,14 +1,9 @@
 class UserMyProfilePage {
 	elements = {
-		getMyProfileTabmenu: () => cy.get('#myTab a[href="/home"]'),
 		getNewPasswordImput: () => cy.get('#password_form_password'),
 		getConfirmNewPassword: () => cy.get('#password_form_password_confirmation'),
 		getChangePasswordBtn: () => cy.get('input[value="Change Password"]'),
 		getAllertMessage: () => cy.get('.panel-body'),
-	};
-
-	clickMyProfileTabmenu() {
-		this.elements.getMyProfileTabmenu().click()
 	};
 
 	clickChangePaswordBtn() {
@@ -16,7 +11,6 @@ class UserMyProfilePage {
 	};
 
 	passwordChange(newpassword) {
-		this.clickMyProfileTabmenu()
 		this.elements.getNewPasswordImput().type(newpassword, { log: false })
 		this.elements.getConfirmNewPassword().type(newpassword, { log: false })
 		this.clickChangePaswordBtn()
