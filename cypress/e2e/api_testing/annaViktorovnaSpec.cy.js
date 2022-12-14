d91be895490cc30f8cc9449ef86b44c2b6ddfadc
AnnaViktorovna <anna.bazileeva@gmail.com>
2022-12-14 18:02:19 -0500

    API test2 changes

--- cypress/e2e/api_testing/annaViktorovnaSpec.cy.js
+++ cypress/e2e/api_testing/annaViktorovnaSpec.cy.js
@@ -2,6 +2,7 @@
 
 const API_BASE_URL = Cypress.env("apiBaseUrl");
 const apiData = require("../../fixtures/apiData.json");
+
 const VALIDATE_DATE = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
 
 describe("API testing with Cypress", () => {
